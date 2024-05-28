159451e7019571aa24df885ba1ecb166348e87ba
theoliverlear <sigwartholiver2@gmail.com>
2024-05-27 21:56:39 -0500

    Correct SecurityConfig.java class annotation from @Controller to @Configuration

--- src/main/java/org/theoliverlear/config/SecurityConfig.java
+++ src/main/java/org/theoliverlear/config/SecurityConfig.java
@@ -1,11 +1,11 @@
 package org.theoliverlear.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Controller;
 
-@Controller
+@Configuration
 public class SecurityConfig {
     //------------------------Security-Filter-Chain---------------------------
     @Bean
