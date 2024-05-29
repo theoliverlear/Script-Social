05045681136b13c119fb2af397fb0f9681ae5d87
theoliverlear <sigwartholiver2@gmail.com>
2024-05-28 21:42:25 -0500

    Refactor User.java to include user attributes and constructors

--- src/main/java/org/theoliverlear/entity/user/User.java
+++ src/main/java/org/theoliverlear/entity/user/User.java
@@ -1,4 +1,63 @@
 package org.theoliverlear.entity.user;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.theoliverlear.entity.Post;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @AttributeOverride(
+            name = "encodedPassword",
+            column = @Column(name = "password"))
+    @Embedded
+    private SafePassword safePassword;
+    @OneToMany(mappedBy = "poster", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    List<Post> posts;
+    public User() {
+        this.username = "";
+        this.email = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.safePassword = new SafePassword();
+    }
+    public User(String username, String unencodedPassword) {
+        this.username = username;
+        this.email = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.safePassword = new SafePassword(unencodedPassword);
+    }
+    public User(String username, String email, String firstName, String lastName, SafePassword safePassword) {
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.safePassword = safePassword;
+    }
+    public User(String username, String email, String firstName, String lastName, SafePassword safePassword, List<Post> posts) {
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.safePassword = safePassword;
+        this.posts = posts;
+    }
 }
