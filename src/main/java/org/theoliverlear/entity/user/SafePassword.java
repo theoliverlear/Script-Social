8cef28dcaf7d7e1b79d0f55e0aa292c9d09b027e
theoliverlear <sigwartholiver2@gmail.com>
2024-05-28 21:42:03 -0500

    Add password encoding functionality to SafePassword.java

--- src/main/java/org/theoliverlear/entity/user/SafePassword.java
+++ src/main/java/org/theoliverlear/entity/user/SafePassword.java
@@ -1,4 +1,28 @@
 package org.theoliverlear.entity.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Transient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Embeddable
 public class SafePassword {
+    String encodedPassword;
+    @Transient
+    @JsonIgnore
+    BCryptPasswordEncoder encoder;
+    public SafePassword() {
+        this.encodedPassword = "";
+        this.encoder = new BCryptPasswordEncoder();
+    }
+    public SafePassword(String unencodedPassword) {
+        this.encoder = new BCryptPasswordEncoder();
+        this.encodedPassword = this.encodePassword(unencodedPassword);
+    }
+    public String encodePassword(String unencodedPassword) {
+        return this.encoder.encode(unencodedPassword);
+    }
+    public boolean compareUnencodedPassword(String unencodedPassword) {
+        return this.encoder.matches(unencodedPassword, this.encodedPassword);
+    }
 }
