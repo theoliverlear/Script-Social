b7aa73d0c8997dd40ea746526cb30443fd1aa37b
theoliverlear <sigwartholiver2@gmail.com>
2024-05-30 18:07:40 -0500

    Add new request mapping method to handle profile ID queries in ProfileController.java

--- src/main/java/org/theoliverlear/controller/ProfileController.java
+++ src/main/java/org/theoliverlear/controller/ProfileController.java
@@ -1,8 +1,11 @@
 package org.theoliverlear.controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.communication.response.ProfileResponse;
 
 @RequestMapping("/profile")
 @Controller
@@ -13,5 +16,14 @@ public class ProfileController {
             return "redirect:/";
         }
         return "profile";
+    }
+    @RequestMapping("/{id}")
+    public ResponseEntity<ProfileResponse> profile(HttpSession session, String id) {
+        if (session.getAttribute("user") == null) {
+            // Response 401 is the
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        ProfileResponse response = new ProfileResponse(session.getAttribute("user").equals(id));
+        return ResponseEntity.ok(response);
     }
 }
