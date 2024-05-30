9d7fe072305763f4effacd24aafd7cf1cbe4de3d
theoliverlear <sigwartholiver2@gmail.com>
2024-05-30 18:09:50 -0500

    Add WelcomeProfile.ts to manage user profile on welcome interface

---
+++ src/main/resources/static/script/WelcomeProfile.ts
@@ -0,0 +1,10 @@
+import {ProfileIntention} from "./ProfileIntention";
+
+export class WelcomeProfile {
+    private _firstName: string;
+    private _lastName: string;
+    private _interests: string[];
+    private _employment: string;
+    private _profileIntention: ProfileIntention;
+
+}
