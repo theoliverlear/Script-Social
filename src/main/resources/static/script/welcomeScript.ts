67dfe5dd228ecff53bdab06e82f99d20d895e05c
theoliverlear <sigwartholiver2@gmail.com>
2024-05-30 19:09:44 -0500

    Refactor welcomeScript.ts to improve interaction, including animations and input handling

--- src/main/resources/static/script/welcomeScript.ts
+++ src/main/resources/static/script/welcomeScript.ts
@@ -1,6 +1,8 @@
-import {loadPage} from "./globalScript";
+import {loadPage, typeText} from "./globalScript";
 import {WelcomeProfile} from "./WelcomeProfile";
 
+let body: JQuery<HTMLElement> = $('body');
+let inputsSection = $('#inputs-section');
 let firstInput = $('#first-input');
 let welcomeButtons = $('#welcome-buttons');
 let secondInput = $('#second-input');
@@ -9,6 +11,8 @@ let backButton = $('#back-button');
 let interestItems = $('.interest-item');
 let interestItemsSelected: JQuery<Element>[] = [];
 let builtProfile: WelcomeProfile = new WelcomeProfile();
+let titleQuestionDiv = $('#title-question-div');
+let titleQuestionText = $('#title-question-text');
 let titleSection = $('#title-section');
 let currentQuestionIndex: number = 0;
 function showCorrectButton() {
@@ -20,12 +24,27 @@ function showCorrectButton() {
         welcomeButtons.css('justify-content', 'space-between');
     }
 }
-
-function floatTitleSectionFromBottom() {
-    titleSection.css('bottom', '0');
-
+function showCorrectInputsAndTitle() {
+    switch (currentQuestionIndex) {
+        case 0:
+            nameSection();
+    }
 }
-
+async function floatTitleSectionFromBottom(): Promise<void> {
+    return new Promise((resolve) => {
+        body.css('overflow-y', 'hidden');
+        let bottom = $(window).height() - titleSection.height();
+        titleSection.css({position: 'relative', bottom: -bottom});
+        titleSection.animate({bottom: 0}, 2500).promise().done(function () {
+            body.css('overflow-y', 'auto');
+            resolve();
+        });
+    });
+}
+function showInputsSection() {
+    inputsSection.fadeIn();
+    //titleSection.fadeOut();
+}
 function nameSection() {
     let bothInputsFilled: boolean = !hasEmptyInput(firstInput) && !hasEmptyInput(secondInput);
 
@@ -60,7 +79,9 @@ function moveSelectionForward() {
 function moveSelectionBackward() {
 
 }
-
+async function typeSectionQuestion() {
+    await typeText(titleQuestionText, titleQuestions[currentQuestionIndex]);
+}
 // Maybe add "What features would you like to see?" as a question.
 let titleQuestions: string[] = ['What\'s your name?', 'Select your interests.'];
 
@@ -69,6 +90,9 @@ if (shouldLoadPage) {
     interestItems.on('click', selectInterestItem);
     confirmButton.on('click', confirmSelection);
     backButton.on('click', backSelection);
-    floatTitleSectionFromBottom();
-    showCorrectButton();
+    floatTitleSectionFromBottom().then(async () => {
+        showInputsSection();
+        await typeSectionQuestion();
+        showCorrectButton();
+    });
 }
