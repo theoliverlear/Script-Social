503ce1253a4c9fa8500376671fe3ed2d363c94fa
theoliverlear <sigwartholiver2@gmail.com>
2024-05-31 15:31:28 -0500

    Add EmploymentStatus.ts to represent employment status options in static scripts

---
+++ src/main/resources/static/script/EmploymentStatus.ts
@@ -0,0 +1,9 @@
+export enum EmploymentStatus {
+    //============================-Variables-=================================
+    INDEPENDENT = 'Independent',
+    EMPLOYED = 'Employed',
+    SEEKING_EMPLOYMENT = 'Seeking Employment',
+    STUDENT = 'Student',
+    HOBBYIST = 'Hobbyist',
+    BUILDING_TEAM = 'Building Team'
+}
