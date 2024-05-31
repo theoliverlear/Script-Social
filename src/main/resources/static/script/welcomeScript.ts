01d7d0db78d700c9d976022368959827fc4294cd
theoliverlear <sigwartholiver2@gmail.com>
2024-05-31 15:26:49 -0500

    Refactor and enhance welcomeScript.ts by adding type definitions, improving function names, and adding more comments for readability

--- src/main/resources/static/script/welcomeScript.ts
+++ src/main/resources/static/script/welcomeScript.ts
@@ -1,23 +1,42 @@
+//=================================-Imports-==================================
 import {loadPage, typeText} from "./globalScript";
 import {WelcomeProfile} from "./WelcomeProfile";
 import confetti from 'canvas-confetti';
+import {PromptMovement} from "./PromptMovement";
+//================================-Variables-=================================
 
+//------------------------------General-Content-------------------------------
 let body: JQuery<HTMLElement> = $('body');
-let inputsSection = $('#inputs-section');
-let firstInput = $('#first-input');
-let welcomeButtons = $('#welcome-buttons');
-let secondInput = $('#second-input');
-let confirmButton = $('#confirm-button');
-let backButton = $('#back-button');
-let interestItems = $('.interest-item');
+//-----------------------------------Inputs-----------------------------------
+let inputsSection: JQuery<HTMLElement> = $('#inputs-section');
+let firstInput: JQuery<HTMLElement> = $('#first-input');
+let secondInput: JQuery<HTMLElement> = $('#second-input');
+//----------------------------------Buttons-----------------------------------
+let welcomeButtons: JQuery<HTMLElement> = $('#welcome-buttons');
+let confirmButton: JQuery<HTMLElement> = $('#confirm-button');
+let backButton: JQuery<HTMLElement> = $('#back-button');
+//------------------------------Interest-Section------------------------------
+let interestItems: JQuery<HTMLElement> = $('.interest-item');
 let interestItemsSelected: JQuery<Element>[] = [];
-let builtProfile: WelcomeProfile = new WelcomeProfile();
-let titleQuestionDiv = $('#title-question-div');
-let titleQuestionText = $('#title-question-text');
-let titleSection = $('#title-section');
+//-------------------------------Title-Section--------------------------------
+let titleQuestionDiv: JQuery<HTMLElement> = $('#title-question-div');
+let titleQuestionText: JQuery<HTMLElement> = $('#title-question-text');
+let titleSection: JQuery<HTMLElement> = $('#title-section');
 let currentQuestionIndex: number = 0;
+let titleQuestions: string[] = ['What\'s your name?',
+                                'When were you born?',
+                                'Select your interests.',
+                                'How do you plan to use Script Social?'];
+//-------------------------------Popup-Content--------------------------------
+let consolePopupContainer: JQuery<HTMLElement> = $('#console-popup-container');
+let consolePopupText: JQuery<HTMLElement> = $('#console-popup-text');
+//----------------------------------Profile-----------------------------------
+let builtProfile: WelcomeProfile = new WelcomeProfile();
+//=============================-Server-Functions-=============================
 
-let titleQuestions: string[] = ['What\'s your name?', 'Select your interests.', "How do you plan to use Script Social?"];
+//=============================-Client-Functions-=============================
+
+//----------------------------Show-Correct-Button-----------------------------
 function showCorrectButton() {
     if (currentQuestionIndex === 0) {
         backButton.hide();
@@ -27,12 +46,17 @@ function showCorrectButton() {
         welcomeButtons.css('justify-content', 'space-between');
     }
 }
-function showCorrectInputsAndTitle() {
+//-----------------------Show-Correct-Inputs-And-Title------------------------
+function showCorrectInputsAndTitle(): PromptMovement {
+    let promptMovement: PromptMovement;
     switch (currentQuestionIndex) {
         case 0:
-            nameSection();
+            promptMovement = showNameSection();
+            break;
     }
+    return promptMovement;
 }
+//----------------------Float-Title-Section-From-Bottom-----------------------
 async function floatTitleSectionFromBottom(): Promise<void> {
     return new Promise((resolve) => {
         body.css('overflow-y', 'hidden');
@@ -44,17 +68,40 @@ async function floatTitleSectionFromBottom(): Promise<void> {
         });
     });
 }
-function showInputsSection() {
+//----------------------------Show-Inputs-Section-----------------------------
+function showInputsSection(): void {
     inputsSection.fadeIn();
     //titleSection.fadeOut();
 }
-function nameSection() {
+//-----------------------------Show-Name-Section------------------------------
+function showNameSection(): PromptMovement {
     let bothInputsFilled: boolean = !hasEmptyInput(firstInput) && !hasEmptyInput(secondInput);
-
+    if (bothInputsFilled) {
+        if (consolePopupContainer.is(':visible')) {
+            hidePopupMessage()
+        }
+        builtProfile.setFirstName(String(firstInput.val()));
+        builtProfile.setLastName(String(secondInput.val()));
+        return PromptMovement.FORWARD;
+    } else {
+        showPopupMessage('Please fill out both inputs.');
+        return PromptMovement.NO_CHANGE;
+    }
 }
+//-----------------------------Show-Popup-Message-----------------------------
+function showPopupMessage(message: string): void {
+    consolePopupText.text(message);
+    consolePopupContainer.fadeIn();
+}
+//-----------------------------Hide-Popup-Message-----------------------------
+function hidePopupMessage(): void {
+    consolePopupContainer.fadeOut();
+}
+//------------------------------Has-Empty-Input-------------------------------
 function hasEmptyInput(inputElement: JQuery<HTMLElement>): boolean {
     return inputElement.val() === '';
 }
+//----------------------------Select-Interest-Item----------------------------
 function selectInterestItem(): void {
     let interestItem: JQuery<any> = $(this);
     if (interestItem.hasClass('general-select')) {
@@ -64,27 +111,38 @@ function selectInterestItem(): void {
         interestItem.fadeIn().toggleClass('general-select');
     }
 }
+//---------------------------Deselect-Interest-Item---------------------------
 function deselectInterestItem(element: JQuery<Element>): void {
     element.toggleClass('general-select');
     interestItemsSelected = interestItemsSelected.filter((item: JQuery<Element>): boolean => {
         return item.text() === element.text();
     });
 }
-function confirmSelection(): void {
-
+//---------------------------Confirm-Button-Pressed---------------------------
+function confirmButtonPressed(): void {
+    moveSection(PromptMovement.FORWARD);
 }
-function backSelection() {
+//----------------------------Back-Button-Pressed-----------------------------
+function backButtonPressed(): void {
+    moveSection(PromptMovement.BACKWARD);
+}
+//--------------------------------Move-Section--------------------------------
+function moveSection(promptMovement: PromptMovement): void {
+    if (promptMovement === PromptMovement.BACKWARD) {
+        currentQuestionIndex--;
+        typeSectionQuestion();
+    }
+    if (showCorrectInputsAndTitle() === PromptMovement.FORWARD) {
+        currentQuestionIndex++;
+        typeSectionQuestion();
+    }
     showCorrectButton();
 }
-function moveSelectionForward() {
-
-}
-function moveSelectionBackward() {
-
-}
-async function typeSectionQuestion() {
+//--------------------------------Move-Section--------------------------------
+async function typeSectionQuestion(): Promise<void> {
     await typeText(titleQuestionText, titleQuestions[currentQuestionIndex]);
 }
+//------------------------------Confetti-Bursts-------------------------------
 async function confettiBursts(): Promise<void> {
     return new Promise(resolve => {
         confetti({
@@ -96,18 +154,21 @@ async function confettiBursts(): Promise<void> {
         });
     });
 }
-
-let shouldLoadPage = loadPage(document.body, 'welcome');
+//================================-Init-Load-=================================
+let shouldLoadPage: boolean = loadPage(document.body, 'welcome');
 if (shouldLoadPage) {
-    interestItems.on('click', selectInterestItem);
-    confirmButton.on('click', confirmSelection);
-    backButton.on('click', backSelection);
     showCorrectButton();
     confettiBursts();
-    floatTitleSectionFromBottom().then(async () => {
+    floatTitleSectionFromBottom().then(async (): Promise<void> => {
         showInputsSection();
         await new Promise(resolve => setTimeout(resolve, 250));
         await typeSectionQuestion();
 
     });
+}
+//=============================-Event-Listeners-==============================
+if (shouldLoadPage) {
+    interestItems.on('click', selectInterestItem);
+    confirmButton.on('click', confirmButtonPressed);
+    backButton.on('click', backButtonPressed);
 }
