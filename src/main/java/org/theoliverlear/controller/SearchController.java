dcaffaae572725e3ebf44f45cea787947bad82f4
theoliverlear <sigwartholiver2@gmail.com>
2024-05-30 19:11:18 -0500

    Add SearchController.java for handling search requests

---
+++ src/main/java/org/theoliverlear/controller/SearchController.java
@@ -0,0 +1,9 @@
+package org.theoliverlear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+}
