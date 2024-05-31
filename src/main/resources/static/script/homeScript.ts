c898b0b30f07281f674caf5137bfbbf5a9bcfb9b
theoliverlear <sigwartholiver2@gmail.com>
2024-05-31 15:30:33 -0500

    Refactor and reorganize code in homeScript.ts for clearer structure and readability

--- src/main/resources/static/script/homeScript.ts
+++ src/main/resources/static/script/homeScript.ts
@@ -1,27 +1,23 @@
+//=================================-Imports-==================================
 import {loadPage, typeText} from "./globalScript";
+//================================-Variables-=================================
 
+//-------------------------------Title-Content--------------------------------
 let titleBlinkingCursor: JQuery<HTMLElement> = $('#title-blinking-cursor');
 let titleText: JQuery<HTMLElement> = $('#title-text');
-let titleString: string = 'Script Social';
+const titleString: string = 'Script Social';
+//-----------------------------------Popup------------------------------------
 let popupAndConfirmDiv = $('#popup-and-confirm-div');
-let popupDiv = $('#popup-div');
-let popupText = $('#popup-text');
-function typeTitleText(): void {
-    // titleText.addClass('being-typed');
-    typeText(titleText, titleString).then(function(): void {
-        // titleText.removeClass('being-typed');
-        //titleBlinkingCursor.fadeIn(100);
-    });
-}
-function applyPopupAndConfirmDivContainer() {
-    if (popupDiv.css('display') === 'none') {
-        popupAndConfirmDiv.css('justify-content', 'flex-end');
-    } else {
-        popupAndConfirmDiv.css('justify-content', 'space-between');
-    }
-}
-let usernameInput = $('#join-console-username-input');
-let passwordInput = $('#join-console-password-input');
+let popupDiv: JQuery<HTMLElement> = $('#popup-div');
+let popupText: JQuery<HTMLElement> = $('#popup-text');
+//-----------------------------------Inputs-----------------------------------
+// TODO: Add the rest of the inputs like email and confirm password.
+let usernameInput: JQuery<HTMLElement> = $('#join-console-username-input');
+let passwordInput: JQuery<HTMLElement> = $('#join-console-password-input');
+let confirmJoinButton: JQuery<HTMLElement> = $('#confirm-join-button');
+//=============================-Server-Functions-=============================
+
+//---------------------------Send-Signup-To-Server----------------------------
 function sendSignupToServer() {
     fetch('/authorize/signup', {
         method: 'POST',
@@ -35,7 +31,6 @@ function sendSignupToServer() {
     }).then(response => response.json()).then(responseJson => {
         console.log(responseJson);
         let isAuthorized: boolean = responseJson.authorized;
-        console.log('Is authorized: ', isAuthorized);
         if (isAuthorized) {
             window.location.href = '/profile/';
         } else {
@@ -46,9 +41,31 @@ function sendSignupToServer() {
         console.error('Error: ', error);
     });
 }
-let confirmJoinButton = $('#confirm-join-button');
-if (loadPage(document.body, 'home')) {
+//=============================-Client-Functions-=============================
+
+//------------------------------Type-Title-Text-------------------------------
+function typeTitleText(): void {
+    // titleText.addClass('being-typed');
+    typeText(titleText, titleString).then(function(): void {
+        // titleText.removeClass('being-typed');
+        //titleBlinkingCursor.fadeIn(100);
+    });
+}
+//-------------------Apply-Popup-And-Confirm-Div-Container--------------------
+function applyPopupAndConfirmDivContainer() {
+    if (popupDiv.css('display') === 'none') {
+        popupAndConfirmDiv.css('justify-content', 'flex-end');
+    } else {
+        popupAndConfirmDiv.css('justify-content', 'space-between');
+    }
+}
+//================================-Init-Load-=================================
+let shouldLoadPage: boolean = loadPage(document.body, 'home');
+if (shouldLoadPage) {
     typeTitleText();
     applyPopupAndConfirmDivContainer();
+}
+//=============================-Event-Listeners-==============================
+if (shouldLoadPage) {
     confirmJoinButton.on('click', sendSignupToServer);
 }
