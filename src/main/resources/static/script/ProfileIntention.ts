b1604dc007d3226a427feac32181dfcbaf81aac6
theoliverlear <sigwartholiver2@gmail.com>
2024-05-31 15:29:37 -0500

    Add SOCIALIZE option to ProfileIntention enum in ProfileIntention.ts

--- src/main/resources/static/script/ProfileIntention.ts
+++ src/main/resources/static/script/ProfileIntention.ts
@@ -1,6 +1,8 @@
 export enum ProfileIntention {
+    //============================-Variables-=================================
     NETWORKING = 'Networking',
     GETTING_INVOLVED = 'Getting Involved',
     CONNECT_TEAM = 'Connect with Team',
-    FIND_TEAM = 'Find a Team'
+    FIND_TEAM = 'Find a Team',
+    SOCIALIZE = 'Socialize'
 }
