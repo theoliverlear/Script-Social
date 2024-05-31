f39384c9fd66527179eef87c63f114963f763b58
theoliverlear <sigwartholiver2@gmail.com>
2024-05-31 15:31:03 -0500

    Refactor code, add function comments, and improve readability in globalScript.ts

--- src/main/resources/static/script/globalScript.ts
+++ src/main/resources/static/script/globalScript.ts
@@ -1,15 +1,27 @@
+//================================-Variables-=================================
+
+//------------------------------General-Content-------------------------------
+let body = $('body');
+const allImages: HTMLCollectionOf<HTMLImageElement> = document.getElementsByTagName('img');
+//----------------------------------Nav-Bar-----------------------------------
 let hamburgerMenuDiv = $('#nav-hamburger-menu');
-// TODO: Have each nav bar item come in one at a time from the right.
 let navItems = $('.nav-item');
 let navBar = $('#nav-bar');
-let body = $('body');
 let navBarDisplayed: boolean = false;
 let navBarItemsTexts: string[] = ['Home', 'Search', 'Account'];
-// Have an option where the options are typed in one at a time.
-const images: HTMLCollectionOf<HTMLImageElement> = document.getElementsByTagName('img');
-for (let image of Array.from(images)) {
-    image.draggable = false;
+
+
+//=============================-Server-Functions-=============================
+
+//=============================-Client-Functions-=============================
+
+//--------------------------Disable-Draggable-Images--------------------------
+function disableDraggableImages(): void {
+    for (let image of Array.from(allImages)) {
+        image.draggable = false;
+    }
 }
+//-----------------------Toggle-Desktop-Typed-Nav-Text------------------------
 async function toggleDesktopTypedNavText(): Promise<void> {
     if (!navBarDisplayed) {
         displayNavBar();
@@ -26,7 +38,7 @@ async function toggleDesktopTypedNavText(): Promise<void> {
             element.find('.nav-text').removeClass('being-typed');
         }
     } else {
-        for(let i = 0; i < navItems.length; i++) {
+        for (let i: number = 0; i < navItems.length; i++) {
             let element = $(navItems[i]);
             element.find('.nav-text').removeClass('typing-completed');
             await deleteText(element.find('.nav-text'));
@@ -36,6 +48,7 @@ async function toggleDesktopTypedNavText(): Promise<void> {
         hideNavBar();
     }
 }
+//---------------------------------Type-Text----------------------------------
 async function typeText(element: JQuery<HTMLElement>, text: string, speed: number = 150): Promise<void> {
     let currentCharIndex = 0;
     return new Promise((resolve) => {
@@ -43,7 +56,7 @@ async function typeText(element: JQuery<HTMLElement>, text: string, speed: numbe
             if (currentCharIndex < text.length) {
                 element.append(text.charAt(currentCharIndex));
                 currentCharIndex++;
-                setTimeout(typeChar, 150);
+                setTimeout(typeChar, speed);
             } else {
                 resolve();
             }
@@ -51,13 +64,14 @@ async function typeText(element: JQuery<HTMLElement>, text: string, speed: numbe
         typeChar();
     });
 }
+//--------------------------------Delete-Text---------------------------------
 async function deleteText(element: JQuery<HTMLElement>, speed: number = 150): Promise<void> {
-    return new Promise((resolve) => {
+    return new Promise((resolve): void => {
         function deleteChar(): void {
             let text = element.text();
             if (text.length > 0) {
                 element.text(text.slice(0, -1));
-                setTimeout(deleteChar, 150);
+                setTimeout(deleteChar, speed);
             } else {
                 resolve();
             }
@@ -65,9 +79,11 @@ async function deleteText(element: JQuery<HTMLElement>, speed: number = 150): Pr
         deleteChar();
     });
 }
+//-------------------------------Is-Mobile-View-------------------------------
 function isMobileView(): boolean {
     return window.innerWidth < 500;
 }
+//------------------------------Display-Nav-Bar-------------------------------
 function displayNavBar() {
     if (isMobileView()) {
 
@@ -77,6 +93,7 @@ function displayNavBar() {
         navBar.fadeIn(200).css('margin-right', 'calc(((3vw + 3vh) / 2) + 1.2em)');
     }
 }
+//--------------------------------Hide-Nav-Bar--------------------------------
 function hideNavBar() {
     if (isMobileView()) {
 
@@ -86,6 +103,7 @@ function hideNavBar() {
         navBar.fadeOut(200).css('margin-right', '0');
     }
 }
+//------------------------------Toggle-Nav-Items------------------------------
 function toggleNavItems(): void {
     if (isMobileView()) {
         toggleMobileNavItems();
@@ -94,6 +112,7 @@ function toggleNavItems(): void {
         toggleDesktopTypedNavText();
     }
 }
+//--------------------------Toggle-Mobile-Nav-Items---------------------------
 function toggleMobileNavItems(): void {
     if (navBar.css('display') === 'none') {
         navBar.fadeIn(200).css('display', 'flex');
@@ -101,39 +120,41 @@ function toggleMobileNavItems(): void {
         navBar.fadeOut(200).css('display', 'none');
     }
 }
-
+//--------------------------Toggle-Desktop-Nav-Items--------------------------
 function toggleDesktopNavItems(): void {
+    let navBarItem: JQuery<any> = $(this);
     if (!navBarDisplayed) {
         displayNavBar();
         body.css('overflow', 'hidden');
-        navItems.each(function (index) {
-            let right = $(window).width() - ($(this).offset().left + $(this).outerWidth());
-            $(this).css({position: 'relative', right: -right});
-            $(this).css('display', 'flex');
-            $(this).animate({right: '0px'}, 1000 * (index + 1));
-        }).promise().done(function () {
+        navItems.each(function (index: number): void {
+            let rightOffset: number = $(window).width() - (navBarItem.offset().left + navBarItem.outerWidth());
+            navBarItem.css({position: 'relative', right: -rightOffset});
+            navBarItem.css('display', 'flex');
+            navBarItem.animate({right: '0px'}, 1000 * (index + 1));
+        }).promise().done(function(): void {
             body.css('overflow', 'auto');
         });
     } else {
         body.css('overflow', 'hidden');
-        let navItemsCount = navItems.length;
-
-        navItems.each(function(index) {
+        let navItemsCount: number = navItems.length;
+        navItems.each(function(index: number): void {
             // Calculate distance to move item offscreen from right side
-            let left = $(window).width();
-
+            let leftOffset: number = $(window).width();
             // Move item offscreen to right using 'left' CSS property
-            $(this).animate({ left: left }, 500 * (navItemsCount - index));
-        }).promise().done(function() {
+            navBarItem.animate({ left: leftOffset }, 500 * (navItemsCount - index));
+        }).promise().done(function(): void {
             hideNavBar();
             body.css('overflow', 'auto');
         });
     }
 }
-
+//================================-Init-Load-=================================
+disableDraggableImages();
 //-------------------------------Load-Page------------------------------------
 function loadPage(bodyElement: HTMLElement, pageName: string): boolean {
     return bodyElement.getAttribute('data-page') === pageName;
 }
-export { loadPage, typeText };
+//=============================-Event-Listeners-==============================
 hamburgerMenuDiv.on('click', toggleNavItems);
+//==================================-Export-==================================
+export { loadPage, typeText };
