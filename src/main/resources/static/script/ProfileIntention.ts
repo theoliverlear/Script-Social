694586a1d10396bc04d2cb340152002448cca2fc
theoliverlear <sigwartholiver2@gmail.com>
2024-06-01 21:14:29 -0500

    Add function to convert string to ProfileIntention enum in ProfileIntention.ts

--- src/main/resources/static/script/ProfileIntention.ts
+++ src/main/resources/static/script/ProfileIntention.ts
@@ -6,3 +6,28 @@ export enum ProfileIntention {
     FIND_TEAM = 'Find a Team',
     SOCIALIZE = 'Socialize'
 }
+export namespace ProfileIntention {
+    export function from(profileIntentionString: string): ProfileIntention {
+        let profileIntention: ProfileIntention;
+        switch(profileIntentionString) {
+            case ProfileIntention.NETWORKING:
+                profileIntention = ProfileIntention.NETWORKING;
+                break;
+            case ProfileIntention.GETTING_INVOLVED:
+                profileIntention = ProfileIntention.GETTING_INVOLVED;
+                break;
+            case ProfileIntention.CONNECT_TEAM:
+                profileIntention = ProfileIntention.CONNECT_TEAM;
+                break;
+            case ProfileIntention.FIND_TEAM:
+                profileIntention = ProfileIntention.FIND_TEAM;
+                break;
+            case ProfileIntention.SOCIALIZE:
+                profileIntention = ProfileIntention.SOCIALIZE;
+                break;
+            default:
+                throw new Error('Invalid Profile Intention');
+        }
+        return profileIntention;
+    }
+}
