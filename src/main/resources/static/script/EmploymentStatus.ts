a7783db2cee0c5c16a1e3275cad49148f9d8e9c0
theoliverlear <sigwartholiver2@gmail.com>
2024-06-01 21:08:45 -0500

    Add 'from' function to convert string to EmploymentStatus in EmploymentStatus.ts

--- src/main/resources/static/script/EmploymentStatus.ts
+++ src/main/resources/static/script/EmploymentStatus.ts
@@ -7,3 +7,31 @@ export enum EmploymentStatus {
     HOBBYIST = 'Hobbyist',
     BUILDING_TEAM = 'Building Team'
 }
+export namespace EmploymentStatus {
+    export function from(employmentStatusString: string): EmploymentStatus {
+        let employmentStatus: EmploymentStatus;
+        switch(employmentStatusString) {
+            case EmploymentStatus.INDEPENDENT:
+                employmentStatus = EmploymentStatus.INDEPENDENT;
+                break;
+            case EmploymentStatus.EMPLOYED:
+                employmentStatus = EmploymentStatus.EMPLOYED;
+                break;
+            case EmploymentStatus.SEEKING_EMPLOYMENT:
+                employmentStatus = EmploymentStatus.SEEKING_EMPLOYMENT;
+                break;
+            case EmploymentStatus.STUDENT:
+                employmentStatus = EmploymentStatus.STUDENT;
+                break;
+            case EmploymentStatus.HOBBYIST:
+                employmentStatus = EmploymentStatus.HOBBYIST;
+                break;
+            case EmploymentStatus.BUILDING_TEAM:
+                employmentStatus = EmploymentStatus.BUILDING_TEAM;
+                break;
+            default:
+                throw new Error('Invalid Employment Status');
+        }
+        return employmentStatus;
+    }
+}
