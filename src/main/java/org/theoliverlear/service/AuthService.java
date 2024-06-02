65677b91959d0e085a60ee694c3cce7a66512b68
theoliverlear <sigwartholiver2@gmail.com>
2024-06-02 18:57:53 -0500

    Refactor AuthService.java by replacing UserRepository with UserService for user handling operations

--- src/main/java/org/theoliverlear/service/AuthService.java
+++ src/main/java/org/theoliverlear/service/AuthService.java
@@ -1,44 +1,42 @@
 package org.theoliverlear.service;
 
 import jakarta.servlet.http.HttpSession;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.theoliverlear.communication.request.AuthRequest;
 import org.theoliverlear.entity.user.User;
-import org.theoliverlear.repository.UserRepository;
 
 @Service
+@Getter
 public class AuthService {
-    private UserRepository userRepository;
+    private UserService userService;
     @Autowired
-    public AuthService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public AuthService(UserService userService) {
+        this.userService = userService;
     }
     public boolean signup(AuthRequest authRequest, HttpSession session) {
-        if (this.userExistsByUsername(authRequest.getUsername())) {
+        if (this.userService.userExistsByUsername(authRequest.getUsername())) {
             return false;
         }
         User user = new User(authRequest.getUsername(), authRequest.getPassword());
-        this.userRepository.save(user);
-        User userWithId = this.userRepository.findByUsername(authRequest.getUsername());
+        this.userService.saveUser(user);
+        User userWithId = this.userService.findByUsername(authRequest.getUsername());
         session.setAttribute("user", userWithId);
         return true;
     }
     public boolean login(AuthRequest authRequest, HttpSession session) {
-        if (!this.userExistsByUsername(authRequest.getUsername())) {
+        if (!this.userService.userExistsByUsername(authRequest.getUsername())) {
             return false;
         }
-        User user = this.userRepository.findByUsername(authRequest.getUsername());
+        User user = this.userService.findByUsername(authRequest.getUsername());
         boolean isAuthorized = user.getSafePassword().compareUnencodedPassword(authRequest.getPassword());
         if (isAuthorized) {
             session.setAttribute("user", user);
         }
         return isAuthorized;
     }
-    public boolean userExistsByUsername(String username) {
-        return userRepository.existsByUsername(username);
-    }
+
     public boolean logout(HttpSession session) {
         if (session.getAttribute("user") == null) {
             return false;
@@ -48,4 +46,5 @@ public class AuthService {
             return true;
         }
     }
+
 }
