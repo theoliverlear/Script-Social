00c3d274b50c07b66bbe8993a9cfe1e58d343212
theoliverlear <sigwartholiver2@gmail.com>
2024-06-02 19:37:03 -0500

    Refactor Post import path in PostRepository.java to use updated package name

--- src/main/java/org/theoliverlear/repository/PostRepository.java
+++ src/main/java/org/theoliverlear/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package org.theoliverlear.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.theoliverlear.entity.Post;
+import org.theoliverlear.entity.content.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
