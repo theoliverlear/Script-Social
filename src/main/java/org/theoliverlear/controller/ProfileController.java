bf6917249961780a33d1badb79952eba818362b2
theoliverlear <sigwartholiver2@gmail.com>
2024-06-02 19:47:40 -0500

    Refactor profile method in ProfileController.java to return a model and view instead of a ResponseEntity

--- src/main/java/org/theoliverlear/controller/ProfileController.java
+++ src/main/java/org/theoliverlear/controller/ProfileController.java
@@ -4,8 +4,11 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.theoliverlear.communication.response.ProfileResponse;
+import org.theoliverlear.entity.user.User;
 
 @RequestMapping("/profile")
 @Controller
@@ -18,12 +21,17 @@ public class ProfileController {
         return "profile";
     }
     @RequestMapping("/{id}")
-    public ResponseEntity<ProfileResponse> profile(HttpSession session, String id) {
-        if (session.getAttribute("user") == null) {
-            // Response 401 is the
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    public String profileId(HttpSession session, @PathVariable String id, Model model) {
+        User sessionUser = (User) session.getAttribute("user");
+        if (sessionUser == null) {
+           return "redirect:/";
         }
-        ProfileResponse response = new ProfileResponse(session.getAttribute("user").equals(id));
-        return ResponseEntity.ok(response);
+        Long idLong = Long.parseLong(id);
+        boolean isMe = sessionUser.getId().equals(idLong);
+        String firstName = sessionUser.getFirstName();
+        String lastName = sessionUser.getLastName();
+        ProfileResponse response = new ProfileResponse(firstName, lastName, isMe);
+        model.addAttribute("profile", response);
+        return "profile";
     }
 }
