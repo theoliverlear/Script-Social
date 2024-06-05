a3f175afc4992ffc1dafb78c4ec24fcd97b87a51
theoliverlear <sigwartholiver2@gmail.com>
2024-06-05 15:40:26 -0500

    Refactor applyWelcomeRequestUserParameters method to handle uppercasing and parsing interests in WelcomeService.java

--- src/main/java/org/theoliverlear/service/WelcomeService.java
+++ src/main/java/org/theoliverlear/service/WelcomeService.java
@@ -13,20 +13,30 @@ import org.theoliverlear.entity.user.personal.ProfileIntention;
 public class WelcomeService {
 
     public User applyWelcomeRequestUserParameters(User user, WelcomeUserRequest welcomeUserRequest) {
-        user.setFirstName(welcomeUserRequest.getFirstName());
-        user.setLastName(welcomeUserRequest.getLastName());
+        // TODO: Make NameFormatter.java class to implement uppercase and
+        //       lowercase functionality.
+        System.out.println(welcomeUserRequest);
+        String firstName = welcomeUserRequest.getFirstName();
+        firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase();
+        user.setFirstName(firstName);
+        String lastName = welcomeUserRequest.getLastName();
+        lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
+        user.setLastName(lastName);
+        System.out.println(welcomeUserRequest.getFirstName() + " " + welcomeUserRequest.getLastName());
         BirthDate birthDate = new BirthDate(welcomeUserRequest.getBirthDate());
         user.setBirthDate(birthDate);
         Interests interests = new Interests();
-        for (String interest : welcomeUserRequest.getInterests()) {
-            interests.addInterest(new Interest(interest));
+        for (String interest : welcomeUserRequest.getInterests().split(",")) {
+            interests.addInterest(new Interest(interest.trim()));
         }
         user.setInterests(interests);
+        interests.setUser(user);
         EmploymentStatus employmentStatus = new EmploymentStatus(welcomeUserRequest.getEmploymentStatus());
         user.setEmploymentStatus(employmentStatus);
         ProfileIntention profileIntention = new ProfileIntention(welcomeUserRequest.getProfileIntention());
         user.setProfileIntention(profileIntention);
         user.setCompletedWelcomeSurvey(true);
+        System.out.println(user);
         return user;
     }
 
