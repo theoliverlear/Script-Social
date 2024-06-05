db11473d7c35cd0fd9528a959ab67be8d3e373aa
theoliverlear <sigwartholiver2@gmail.com>
2024-06-05 15:36:57 -0500

    Add findByUserId and existsByUserId methods to ProfilePictureService.java

--- src/main/java/org/theoliverlear/service/ProfilePictureService.java
+++ src/main/java/org/theoliverlear/service/ProfilePictureService.java
@@ -2,6 +2,7 @@ package org.theoliverlear.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.theoliverlear.entity.user.ProfilePicture;
 import org.theoliverlear.repository.ProfilePictureRepository;
 
@@ -14,5 +15,12 @@ public class ProfilePictureService {
     }
     public void saveProfilePicture(ProfilePicture profilePicture) {
         this.profilePictureRepository.save(profilePicture);
+    }
+    @Transactional
+    public ProfilePicture findByUserId(Long userId) {
+        return this.profilePictureRepository.findByUserId(userId);
+    }
+    public boolean existsByUserId(Long userId) {
+        return this.profilePictureRepository.existsByUserId(userId);
     }
 }
