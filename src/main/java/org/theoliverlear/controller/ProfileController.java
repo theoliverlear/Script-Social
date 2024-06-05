69e1eafa96f084fb2657015ce32802374a9fdeea
theoliverlear <sigwartholiver2@gmail.com>
2024-06-05 15:35:24 -0500

    Add profile picture functionality to ProfileController.java

--- src/main/java/org/theoliverlear/controller/ProfileController.java
+++ src/main/java/org/theoliverlear/controller/ProfileController.java
@@ -1,18 +1,29 @@
 package org.theoliverlear.controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.communication.response.HasProfilePictureResponse;
 import org.theoliverlear.communication.response.ProfileResponse;
+import org.theoliverlear.entity.user.ProfilePicture;
 import org.theoliverlear.entity.user.User;
+import org.theoliverlear.service.ProfilePictureService;
 
 @RequestMapping("/profile")
 @Controller
 public class ProfileController {
+    private ProfilePictureService profilePictureService;
+    @Autowired
+    public ProfileController(ProfilePictureService profilePictureService) {
+        this.profilePictureService = profilePictureService;
+    }
     @RequestMapping("/")
     public String profile(HttpSession session) {
         if (session.getAttribute("user") == null) {
@@ -40,6 +51,23 @@ public class ProfileController {
             return ResponseEntity.ok(new ProfileResponse(sessionUser.getFirstName(), sessionUser.getLastName(), true));
         } else {
             return ResponseEntity.ok(new ProfileResponse("John", "Doe", false));
+        }
+    }
+    @RequestMapping("/get/{id}/has-profile-picture")
+    public ResponseEntity<HasProfilePictureResponse> hasProfilePicture(@PathVariable String id) {
+        boolean hasProfilePicture = this.profilePictureService.existsByUserId(Long.parseLong(id));
+        return ResponseEntity.ok(new HasProfilePictureResponse(hasProfilePicture));
+    }
+    @Transactional
+    @RequestMapping("/get/{id}/profile-picture")
+    public ResponseEntity<byte[]> getProfilePicture(@PathVariable String id) {
+        ProfilePicture profilePicture = this.profilePictureService.findByUserId(Long.parseLong(id));
+        if (profilePicture == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            byte[] imageData = profilePicture.getFileData();
+            String fileType = profilePicture.getFileType();
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(fileType)).body(imageData);
         }
     }
 }
