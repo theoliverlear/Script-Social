b85036a7ce403b47c6ffbd7226475ced8c70db39
theoliverlear <sigwartholiver2@gmail.com>
2024-06-05 15:36:32 -0500

    Add findByUserId and existsByUserId methods in ProfilePictureRepository.java

--- src/main/java/org/theoliverlear/repository/ProfilePictureRepository.java
+++ src/main/java/org/theoliverlear/repository/ProfilePictureRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.user.ProfilePicture;
 
 public interface ProfilePictureRepository extends JpaRepository<ProfilePicture, Long> {
-
+    ProfilePicture findByUserId(Long userId);
+    boolean existsByUserId(Long userId);
 }
