956d5cfcbb2410477c72f33e08134d5e4d5c0159
theoliverlear <sigwartholiver2@gmail.com>
2024-06-07 17:26:34 -0500

    Add bio field to WelcomeUserRequest.java for additional user profile information

--- src/main/java/org/theoliverlear/communication/request/WelcomeUserRequest.java
+++ src/main/java/org/theoliverlear/communication/request/WelcomeUserRequest.java
@@ -12,17 +12,20 @@ public class WelcomeUserRequest {
     private String interests;
     private String employmentStatus;
     private String profileIntention;
+    private String bio;
     public WelcomeUserRequest(String firstName,
                               String lastName,
                               String birthDate,
                               String interests,
                               String employmentStatus,
-                              String profileIntention) {
+                              String profileIntention,
+                              String bio) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
         this.interests = interests;
         this.employmentStatus = employmentStatus;
         this.profileIntention = profileIntention;
+        this.bio = bio;
     }
 }
