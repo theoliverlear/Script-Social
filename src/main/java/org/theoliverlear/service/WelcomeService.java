644276f2ee3c79a6e5fbabf70e98397b48c14672
theoliverlear <sigwartholiver2@gmail.com>
2024-06-07 17:25:49 -0500

    Refactor the WelcomeService.java file to consolidate import statements and add system print statements for debugging, along with additional bio field from user welcome request

--- src/main/java/org/theoliverlear/service/WelcomeService.java
+++ src/main/java/org/theoliverlear/service/WelcomeService.java
@@ -4,10 +4,7 @@ import org.springframework.stereotype.Service;
 import org.theoliverlear.communication.request.WelcomeUserRequest;
 import org.theoliverlear.entity.user.BirthDate;
 import org.theoliverlear.entity.user.User;
-import org.theoliverlear.entity.user.personal.EmploymentStatus;
-import org.theoliverlear.entity.user.personal.Interest;
-import org.theoliverlear.entity.user.personal.Interests;
-import org.theoliverlear.entity.user.personal.ProfileIntention;
+import org.theoliverlear.entity.user.personal.*;
 
 @Service
 public class WelcomeService {
@@ -15,6 +12,7 @@ public class WelcomeService {
     public User applyWelcomeRequestUserParameters(User user, WelcomeUserRequest welcomeUserRequest) {
         // TODO: Make NameFormatter.java class to implement uppercase and
         //       lowercase functionality.
+        System.out.println("Employment Status: " + welcomeUserRequest.getEmploymentStatus());
         System.out.println(welcomeUserRequest);
         String firstName = welcomeUserRequest.getFirstName();
         firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase();
@@ -28,6 +26,7 @@ public class WelcomeService {
         Interests interests = new Interests();
         for (String interest : welcomeUserRequest.getInterests().split(",")) {
             interests.addInterest(new Interest(interest.trim()));
+            System.out.println(interest.trim());
         }
         user.setInterests(interests);
         interests.setUser(user);
@@ -35,6 +34,7 @@ public class WelcomeService {
         user.setEmploymentStatus(employmentStatus);
         ProfileIntention profileIntention = new ProfileIntention(welcomeUserRequest.getProfileIntention());
         user.setProfileIntention(profileIntention);
+        user.setBio(new Bio(welcomeUserRequest.getBio()));
         user.setCompletedWelcomeSurvey(true);
         System.out.println(user);
         return user;
