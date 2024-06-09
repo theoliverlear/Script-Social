e6cb81a4570c8f8e9c44df664189773c9730afa3
theoliverlear <sigwartholiver2@gmail.com>
2024-06-09 11:01:53 -0500

    Refactor ProfilePicture.java to extend Picture class removing duplicate code

--- src/main/java/org/theoliverlear/entity/user/ProfilePicture.java
+++ src/main/java/org/theoliverlear/entity/user/ProfilePicture.java
@@ -3,56 +3,29 @@ package org.theoliverlear.entity.user;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.theoliverlear.entity.content.Picture;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "profile_pictures")
-public class ProfilePicture {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "file_name")
-    private String fileName;
-    @Column(name = "file_type")
-    private String fileType;
-    @Lob
-    @Column(name = "file_data")
-    private byte[] fileData;
+public class ProfilePicture extends Picture {
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
     public ProfilePicture() {
-        this.fileName = "";
-        this.fileData = new byte[0];
+        super();
         this.user = null;
     }
     public ProfilePicture(String fileName, byte[] fileData) {
-        this.fileName = fileName;
-        this.fileData = fileData;
-        this.fetchFileType();
+        super(fileName, fileData);
         this.user = null;
     }
     public ProfilePicture(String fileName, byte[] fileData, User user) {
-        this.fileName = fileName;
-        this.fileData = fileData;
-        this.fetchFileType();
+        super(fileName, fileData);
         this.user = user;
     }
-    public void fetchFileType() {
-        String fileExtension = this.fileName.substring(this.fileName.lastIndexOf(".") + 1);
-        switch (fileExtension) {
-            case "jpg", "jpeg" -> this.fileType = "image/jpeg";
-            case "png" -> this.fileType = "image/png";
-            case "gif" -> this.fileType = "image/gif";
-            case "bmp" -> this.fileType = "image/bmp";
-            case "webp" -> this.fileType = "image/webp";
-            case "svg" -> this.fileType = "image/svg+xml";
-            default -> throw new IllegalArgumentException("Invalid file type");
-        }
-    }
-    public String setFileName(String fileName) {
-        this.fetchFileType();
-        return this.fileName = fileName;
+    public void setFileName(String fileName) {
+        super.setFileName(fileName);
     }
 }
