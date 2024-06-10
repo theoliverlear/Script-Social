bd7f1c5dfa2b70dac40d6eb652ce23d8d46bf429
theoliverlear <sigwartholiver2@gmail.com>
2024-06-10 15:46:02 -0500

    Add method to find all posts by user ID in PostRepository.java

--- src/main/java/org/theoliverlear/repository/PostRepository.java
+++ src/main/java/org/theoliverlear/repository/PostRepository.java
@@ -3,5 +3,8 @@ package org.theoliverlear.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.content.Post;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByPosterId(Long userId);
 }
