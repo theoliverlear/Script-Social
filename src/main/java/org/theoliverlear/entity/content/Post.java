40c255985928d8824e9bcec96ceceb476a4ac6fd
theoliverlear <sigwartholiver2@gmail.com>
2024-06-10 15:45:26 -0500

    Add Comment list to Post.java and implement equals method for comparison of Post instances

--- src/main/java/org/theoliverlear/entity/content/Post.java
+++ src/main/java/org/theoliverlear/entity/content/Post.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.theoliverlear.entity.user.User;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,6 +21,8 @@ public class Post {
     private User poster;
     @Column(name = "content", columnDefinition = "TEXT")
     private String content;
+    @OneToMany(mappedBy="post", cascade = CascadeType.ALL)
+    private List<Comment> comments;
     @Column(name = "time_posted")
     private LocalDateTime timePosted;
     public Post() {
@@ -36,5 +39,23 @@ public class Post {
         this.poster = poster;
         this.content = content;
         this.timePosted = timePosted;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        if (object instanceof Post comparedPost) {
+            boolean samePoster = this.poster.equals(comparedPost.poster);
+            boolean sameContent = this.content.equals(comparedPost.content);
+            boolean sameTimePosted = this.timePosted.equals(comparedPost.timePosted);
+            if (this.id == null) {
+                return samePoster && sameContent && sameTimePosted;
+            } else {
+                boolean sameId = this.id.equals(comparedPost.id);
+                return sameId && samePoster && sameContent && sameTimePosted;
+            }
+        }
+        return false;
     }
 }
