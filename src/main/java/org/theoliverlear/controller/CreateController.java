defd32a7fa02673ad1f138eec978ed8a82d69199
theoliverlear <sigwartholiver2@gmail.com>
2024-06-10 12:42:35 -0500

    Add CreateController.java to handle content creation requests

---
+++ src/main/java/org/theoliverlear/controller/CreateController.java
@@ -0,0 +1,40 @@
+package org.theoliverlear.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.communication.request.PostRequest;
+import org.theoliverlear.communication.response.OperationSuccessfulResponse;
+import org.theoliverlear.entity.content.Post;
+import org.theoliverlear.entity.user.User;
+import org.theoliverlear.service.PostService;
+import org.theoliverlear.service.ScriptSocialService;
+import org.theoliverlear.service.UserService;
+
+@Controller
+@RequestMapping("/create")
+public class CreateController {
+    private ScriptSocialService scriptSocialService;
+    private PostService postService;
+    @Autowired
+    public CreateController(ScriptSocialService scriptSocialService,
+                            UserService userService,PostService postService) {
+        this.scriptSocialService = scriptSocialService;
+        this.postService = postService;
+    }
+    @RequestMapping("/")
+    public String create() {
+        return "create";
+    }
+    public ResponseEntity<OperationSuccessfulResponse> createPost(@RequestBody PostRequest postRequest, HttpSession session) {
+        if (!scriptSocialService.userInSession(session)) {
+            return new ResponseEntity<>(new OperationSuccessfulResponse(false), HttpStatus.UNAUTHORIZED);
+        }
+        boolean postCreated = this.postService.createPost(postRequest);
+        return new ResponseEntity<>(new OperationSuccessfulResponse(postCreated), HttpStatus.OK);
+    }
+}
