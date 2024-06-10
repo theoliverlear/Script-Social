287190afb70562d3321aaca9fe30cfebbd9e17ce
theoliverlear <sigwartholiver2@gmail.com>
2024-06-10 15:46:32 -0500

    Refactor PostService.java to include UserService dependency and add methods for retrieving all posts by a user and deleting a post

--- src/main/java/org/theoliverlear/service/PostService.java
+++ src/main/java/org/theoliverlear/service/PostService.java
@@ -6,12 +6,15 @@ import org.theoliverlear.entity.content.Post;
 import org.theoliverlear.entity.user.User;
 import org.theoliverlear.repository.PostRepository;
 
+import java.util.List;
+
 @Service
 public class PostService {
     private UserService userService;
     private PostRepository postRepository;
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserService userService) {
         this.postRepository = postRepository;
+        this.userService = userService;
     }
     public boolean createPost(PostRequest postRequest) {
         User user = this.userService.getUserById(postRequest.getUserId());
@@ -23,5 +26,20 @@ public class PostService {
         user.addPost(post);
         this.userService.saveUser(user);
         return true;
+    }
+    public List<Post> getAllPostsByPosterId(Long userId) {
+        return this.postRepository.findAllByPosterId(userId);
+    }
+    public boolean deletePost(Long postId) {
+        Post post = this.postRepository.findById(postId).orElse(null);
+        if (post == null) {
+            return false;
+        } else {
+            User user = post.getPoster();
+            user.removePost(post);
+            this.postRepository.deleteById(postId);
+            this.userService.saveUser(user);
+            return true;
+        }
     }
 }
