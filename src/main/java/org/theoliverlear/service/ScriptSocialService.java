96e9c5e623a34aba9515026bc83bc3b149b485bd
theoliverlear <sigwartholiver2@gmail.com>
2024-06-10 12:53:51 -0500

    Add userInSession method to check whether a user is authenticated in a session in ScriptSocialService.java

--- src/main/java/org/theoliverlear/service/ScriptSocialService.java
+++ src/main/java/org/theoliverlear/service/ScriptSocialService.java
@@ -1,7 +1,11 @@
 package org.theoliverlear.service;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScriptSocialService {
+    public boolean userInSession(HttpSession session) {
+        return session.getAttribute("user") != null;
+    }
 }
