bfeb3eba81eaa51fb2b9ea0fea8d7edeb89e5e5b
theoliverlear <sigwartholiver2@gmail.com>
2024-06-10 15:43:35 -0500

    Add Comment.java to represent interactions with user comments

---
+++ src/main/java/org/theoliverlear/entity/content/Comment.java
@@ -0,0 +1,43 @@
+package org.theoliverlear.entity.content;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.theoliverlear.entity.user.User;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User commenter;
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+    @Column(name = "time_posted")
+    private LocalDateTime timePosted;
+    public Comment() {
+        this.commenter = null;
+        this.content = "";
+        this.timePosted = LocalDateTime.now();
+    }
+    public Comment(User commenter, String content) {
+        this.commenter = commenter;
+        this.content = content;
+        this.timePosted = LocalDateTime.now();
+    }
+    public Comment(User commenter, String content, LocalDateTime timePosted) {
+        this.commenter = commenter;
+        this.content = content;
+        this.timePosted = timePosted;
+    }
+}
