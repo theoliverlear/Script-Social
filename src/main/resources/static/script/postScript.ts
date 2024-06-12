2393805df8feebafdb5f42574b12576ea7decf0e
theoliverlear <sigwartholiver2@gmail.com>
2024-06-12 16:35:37 -0500

    Refactor and expand postScript.ts to add new generalized functions and clarify post validation

--- src/main/resources/static/script/postScript.ts
+++ src/main/resources/static/script/postScript.ts
@@ -1,36 +1,58 @@
 // Methods for making posts throughout the program
 
-import {getCurrentUserIdFromServer} from "./globalScript";
+import {Post} from "./Post";
 
-let userPostContent:JQuery<HTMLElement> = $('#write-post-textarea')
-let button :JQuery<HTMLElement> = $('#write-post-button-div')
-
-$(function(){
-    button.on('click', async () => {
-       let userID: number = await getCurrentUserIdFromServer()
-        console.log("user id: ", userID)
-        let contentToSubmit = userPostContent.val()
-        if (isValidPost(contentToSubmit)) {
-            await fetch('/create/post', {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({userID,contentToSubmit})
-            }).catch(err => console.error(err))
-                .then(() => loadPosts())
-        } else {
-            //TODO implement stylized toast for user to see
-            alert("Unable to save empty data!")
+function sendPostToServer(userId: number, message: string) {
+    if (isValidPost(message) && userId !== -1) {
+        console.log('userId: ', userId);
+        console.log('message: ', message);
+        let post: Post = new Post(message, userId);
+        fetch('/create/post', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userId: post.userId,
+                content: post.message
+            })
+        }).catch(error => {
+            console.error('Error: ', error);
+        });
+    }
+}
+async function getPostsFromServer(userId: number): Promise<any> {
+    let response: void | Response = await fetch('/post/get/' + userId, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
         }
-
-    })
-})
-
-function isValidPost(message: string|number|string[]) {
-    return !(message === '' || message === null)
+    }).catch(error => {
+        console.error('Error: ', error);
+    });
+    if (response) {
+        return await response.json();
+    }
+}
+function getPostsFromServerResponse(response: any): Post[] {
+    let postsFromServer: Post[] = [];
+    if (Array.isArray(response)) {
+        response.forEach((postJson: any) => {
+            let post: Post = new Post(postJson.content, postJson.poster,
+                                      postJson.id, postJson.timePosted);
+            postsFromServer.push(post);
+        });
+    }
+    return postsFromServer;
+}
+function isValidPost(message: string): boolean {
+    let messageIsNull: boolean = message === null;
+    let messageIsEmpty: boolean = message === '';
+    return !messageIsNull && !messageIsEmpty;
 }
 
 function loadPosts() {
-    //TODO load posts on successful post save operation
+    //TODO: load posts on successful post save operation
 }
+export { sendPostToServer, getPostsFromServer, getPostsFromServerResponse,
+         isValidPost, loadPosts };
