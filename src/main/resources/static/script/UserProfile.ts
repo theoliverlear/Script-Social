a7d7e6b135724651903e9a4001e1ba4383a8f7d3
theoliverlear <sigwartholiver2@gmail.com>
2024-06-13 14:50:34 -0500

    Refactor UserProfile.ts by encapsulating properties and adding bio attribute

--- src/main/resources/static/script/UserProfile.ts
+++ src/main/resources/static/script/UserProfile.ts
@@ -1,23 +1,32 @@
 export class UserProfile {
-    firstName: string;
-    lastName: string;
-    constructor(firstName: string = '', lastName: string = '') {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    private _firstName: string;
+    private _lastName: string;
+    private _bio: string;
+    constructor(firstName: string = '', lastName: string = '',
+                bio: string = '') {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._bio = bio;
     }
     get fullName(): string {
-        return `${this.firstName} ${this.lastName}`;
+        return `${this._firstName} ${this._lastName}`;
     }
     get getFirstName(): string {
-        return this.firstName;
+        return this._firstName;
     }
     get getLastName(): string {
-        return this.lastName;
+        return this._lastName;
+    }
+    get getBio(): string {
+        return this._bio;
     }
     setFirstName(firstName: string) {
-        this.firstName = firstName;
+        this._firstName = firstName;
     }
     setLastName(lastName: string) {
-        this.lastName = lastName;
+        this._lastName = lastName;
+    }
+    setBio(bio: string) {
+        this._bio = bio;
     }
 }
