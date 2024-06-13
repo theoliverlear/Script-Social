02756add14a13b216dff1eec2ccd1952cfc89e61
theoliverlear <sigwartholiver2@gmail.com>
2024-06-13 14:48:50 -0500

    Refactor profileScript.ts to display user bio and load posts in the user profile

--- src/main/resources/static/script/profileScript.ts
+++ src/main/resources/static/script/profileScript.ts
@@ -1,17 +1,20 @@
 import {getCurrentUserIdFromServer, loadPage} from "./globalScript";
 import {UserProfile} from "./UserProfile";
-import {sendPostToServer} from "./postScript";
+import {loadPosts, sendPostToServer} from "./postScript";
 
 let body: JQuery<HTMLElement> = $('body');
 let userProfile: UserProfile = new UserProfile();
 let userId: number = Number(document.body.getAttribute('user-id'));
 let profilePictureImage: JQuery<HTMLElement> = $('#profile-picture-img');
 
-let firstNameHeadlineText = $('#first-name-headline-text');
-let lastNameHeadlineText = $('#last-name-headline-text');
+let firstNameHeadlineText: JQuery<HTMLElement> = $('#first-name-headline-text');
+let lastNameHeadlineText: JQuery<HTMLElement> = $('#last-name-headline-text');
 
 let writePostButton: JQuery<HTMLElement> = $('#write-post-button-div');
 let writePostTextArea: JQuery<HTMLElement> = $('#write-post-textarea');
+
+let profileBioText: JQuery<HTMLElement> = $('#profile-bio-text');
+let activitySection: JQuery<HTMLElement> = $('#activity-section');
 
 async function makePost(): Promise<void> {
     let postMessage: string = writePostTextArea.val() as string;
@@ -34,7 +37,7 @@ async function getUserDataFromServer(): Promise<void> {
     }
 }
 async function hasProfilePictureFromServer(): Promise<boolean> {
-    let response = await fetch(`/profile/get/${userId}/has-profile-picture`, {
+    let response: Response = await fetch(`/profile/get/${userId}/has-profile-picture`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -47,22 +50,24 @@ async function hasProfilePictureFromServer(): Promise<boolean> {
         return false;
     }
 }
-async function getProfilePictureSequence() {
+async function getProfilePictureSequence(): Promise<void> {
     let hasProfilePicture: boolean = await hasProfilePictureFromServer();
     if (hasProfilePicture) {
         profilePictureImage.attr('src', `/profile/get/${userId}/profile-picture`);
     }
 }
-function buildUserProfile(serverResponse: any) {
+function buildUserProfile(serverResponse: any): void {
     userProfile.setFirstName(serverResponse.firstName);
     userProfile.setLastName(serverResponse.lastName);
+    userProfile.setBio(serverResponse.bio);
     console.log(userProfile.fullName);
     console.log(userProfile.getFirstName);
     console.log(userProfile.getLastName);
 }
-function applyUserProfileToPage() {
+function applyUserProfileToPage(): void {
     firstNameHeadlineText.text(userProfile.getFirstName);
     lastNameHeadlineText.text(userProfile.getLastName);
+    profileBioText.text(userProfile.getBio);
 }
 let shouldLoad: boolean = loadPage(document.body, 'profile');
 if (shouldLoad) {
@@ -70,5 +75,6 @@ if (shouldLoad) {
     getUserDataFromServer().then((): void => {
         applyUserProfileToPage();
     });
+    loadPosts(activitySection, userId);
     writePostButton.on('click', makePost);
 }
