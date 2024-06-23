1be0170480b94f4cec8c33dda974ecb6b52d1a64
theoliverlear <sigwartholiver2@gmail.com>
2024-06-23 12:24:26 -0500

    Add SockJS and Stomp imports and configure WebSocket connection in messageScript.ts

--- src/main/resources/static/script/messageScript.ts
+++ src/main/resources/static/script/messageScript.ts
@@ -1,4 +1,6 @@
 import {loadPage} from "./globalScript";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 
 let connectionItems: JQuery<HTMLElement> = $('.connection-item');
 function addSelectedStyle(): void {
@@ -15,3 +17,13 @@ let shouldLoadPage: boolean = loadPage(document.body, 'message');
 if (shouldLoadPage) {
     connectionItems.on('click', addSelectedStyle);
 }
+let webSocket: WebSocket = new SockJS('/ws');
+let stompClient: any = Stomp.over(webSocket);
+
+stompClient.connect({}, function(frame: any): void {
+    stompClient.subscribe('/message/send/', function(response: any): void {
+        let message: string = JSON.parse(response.body).content;
+        let messageElement: JQuery<HTMLElement> = $('<div class="message-item"></div>').text(message);
+        $('.message-list').append(messageElement);
+    });
+});
