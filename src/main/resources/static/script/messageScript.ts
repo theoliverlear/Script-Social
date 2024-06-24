ddd6dcc0e57068227f558270d04e1f1ddf102c9b
theoliverlear <sigwartholiver2@gmail.com>
2024-06-23 19:16:55 -0500

    Refactor messageScript.ts to use @stomp/stompjs, add a sendMessage function, and modify subscription URL

--- src/main/resources/static/script/messageScript.ts
+++ src/main/resources/static/script/messageScript.ts
@@ -1,6 +1,6 @@
 import {loadPage} from "./globalScript";
 import SockJS from "sockjs-client";
-import Stomp from "stompjs";
+import {Client, Stomp} from "@stomp/stompjs";
 
 let connectionItems: JQuery<HTMLElement> = $('.connection-item');
 function addSelectedStyle(): void {
@@ -13,17 +13,31 @@ function addSelectedStyle(): void {
         $(this).addClass("general-select");
     }
 }
-let shouldLoadPage: boolean = loadPage(document.body, 'message');
-if (shouldLoadPage) {
-    connectionItems.on('click', addSelectedStyle);
-}
+
 let webSocket: WebSocket = new SockJS('/ws');
 let stompClient: any = Stomp.over(webSocket);
 
 stompClient.connect({}, function(frame: any): void {
-    stompClient.subscribe('/message/send/', function(response: any): void {
-        let message: string = JSON.parse(response.body).content;
-        let messageElement: JQuery<HTMLElement> = $('<div class="message-item"></div>').text(message);
-        $('.message-list').append(messageElement);
+    const testId: number = 1;
+    const testMessage: string = 'Test message';
+    stompClient.subscribe(`/message/receiver/${testId}`, function(response: any): void {
+        console.log(response.body);
     });
 });
+function sendMessage(): void {
+    if (userMessageText.val() === '') {
+        return;
+    }
+    const messageInfo: {receiverId: number, message: string} = {
+        receiverId: 1,
+        message: userMessageText.val() as string
+    }
+    stompClient.send('/message/send/', {}, JSON.stringify(messageInfo));
+}
+let userMessageSendButton: JQuery<HTMLElement> = $('#user-message-send-button');
+let userMessageText: JQuery<HTMLElement> = $('#user-message-text');
+let shouldLoadPage: boolean = loadPage(document.body, 'message');
+if (shouldLoadPage) {
+    connectionItems.on('click', addSelectedStyle);
+    userMessageSendButton.on('click', sendMessage);
+}
