c84080711a054cf30761d6923d5d32dcf592c5bb
theoliverlear <sigwartholiver2@gmail.com>
2024-06-23 19:05:44 -0500

    Add ConversationRepository.java to handle queries related to conversations in the database

---
+++ src/main/java/org/theoliverlear/repository/ConversationRepository.java
@@ -0,0 +1,12 @@
+package org.theoliverlear.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.theoliverlear.entity.im.Conversation;
+
+import java.util.List;
+
+public interface ConversationRepository extends JpaRepository<Conversation, Long> {
+    @Query("SELECT c FROM Conversation c JOIN c.subscribers u WHERE u.id IN :userIds")
+    List<Conversation> findByUserIds(List<Long> userIds);
+}
