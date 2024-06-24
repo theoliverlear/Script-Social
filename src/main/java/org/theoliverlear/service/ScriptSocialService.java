b140a28244ed5b628e59a53496b4252932daec82
theoliverlear <sigwartholiver2@gmail.com>
2024-06-23 19:19:44 -0500

    Add getUserFromSession method to retrieve user from a session in ScriptSocialService.java

--- src/main/java/org/theoliverlear/service/ScriptSocialService.java
+++ src/main/java/org/theoliverlear/service/ScriptSocialService.java
@@ -2,10 +2,21 @@ package org.theoliverlear.service;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
+import org.theoliverlear.entity.user.User;
+
+import java.util.Optional;
 
 @Service
 public class ScriptSocialService {
     public boolean userInSession(HttpSession session) {
         return session.getAttribute("user") != null;
+    }
+    public Optional<User> getUserFromSession(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return Optional.empty();
+        } else {
+            return Optional.of(user);
+        }
     }
 }
