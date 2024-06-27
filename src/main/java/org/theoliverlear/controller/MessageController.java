f97acead722f1b3c791211f6a6d763223f2fd1bd
theoliverlear <sigwartholiver2@gmail.com>
2024-06-27 15:10:18 -0500

    Refactor sendMessage method in MessageController.java to handle user session and convert HTTP session to WebSocket session

--- src/main/java/org/theoliverlear/controller/MessageController.java
+++ src/main/java/org/theoliverlear/controller/MessageController.java
@@ -7,11 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.socket.WebSocketSession;
 import org.theoliverlear.communication.request.InstantMessageRequest;
 import org.theoliverlear.communication.response.InstantMessageResponse;
 import org.theoliverlear.communication.response.OperationSuccessfulResponse;
@@ -21,6 +25,7 @@ import org.theoliverlear.entity.user.User;
 import org.theoliverlear.service.ConversationService;
 import org.theoliverlear.service.MessageService;
 import org.theoliverlear.service.ScriptSocialService;
+import org.theoliverlear.service.UserService;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -33,16 +38,20 @@ public class MessageController {
     private ScriptSocialService scriptSocialService;
     private ConversationService conversationService;
     private MessageService messageService;
+    private UserService userService;
+    private WebSocketSession session;
     private User currentUser;
     private SimpMessagingTemplate simpMessagingTemplate;
     @Autowired
     public MessageController(ScriptSocialService scriptSocialService,
                              ConversationService conversationService,
                              MessageService messageService,
+                             UserService userService,
                              SimpMessagingTemplate simpMessagingTemplate) {
         this.scriptSocialService = scriptSocialService;
         this.conversationService = conversationService;
         this.messageService = messageService;
+        this.userService = userService;
         this.simpMessagingTemplate = simpMessagingTemplate;
     }
     @RequestMapping("/")
@@ -86,20 +95,49 @@ public class MessageController {
         return ResponseEntity.ok(messages);
     }
     @MessageMapping("/send")
-    public void sendMessage(@RequestBody InstantMessageRequest instantMessageRequest, HttpSession session) {
-        if (this.scriptSocialService.userInSession(session)) {
+    public void sendMessage(@RequestBody InstantMessageRequest instantMessageRequest, SimpMessageHeaderAccessor headerAccessor) {
+        System.out.println("Received message: " + instantMessageRequest.getMessage());
+        if (!this.scriptSocialService.userInSession(headerAccessor)) {
+            System.out.println("User not in session.");
             return;
         } else {
-            this.currentUser = (User) session.getAttribute("user");
+            Optional<User> possibleCurrentUser = this.scriptSocialService.getUserFromSession(headerAccessor);
+            if (possibleCurrentUser.isEmpty()) {
+                System.out.println("No current user found.");
+                return;
+            } else {
+                System.out.println("Current user found.");
+                this.currentUser = possibleCurrentUser.get();
+            }
         }
+
+        System.out.println("Current user: " + this.currentUser.getUsername());
         Optional<List<Conversation>> conversations = this.conversationService.findByUserIds(this.currentUser.getId(), instantMessageRequest.getReceiverId());
+        Optional<User> possibleReceiver = this.userService.getUserById(instantMessageRequest.getReceiverId());
         if (conversations.isEmpty()) {
+            System.out.println("No conversation found.");
+            if (possibleReceiver.isEmpty()) {
+                System.out.println("No receiver found.");
+                return;
+            } else {
+                User receiver = possibleReceiver.get();
+                Conversation newConversation = this.conversationService.createConversation(this.currentUser, receiver);
+                conversations.get().add(newConversation);
+            }
+        }
+        if (possibleReceiver.isEmpty()) {
+            System.out.println("No receiver found.");
+            return;
+        }
+        if (conversations.get().get(0) == null) {
+            System.out.println("No conversation found.");
             return;
         }
         boolean messageAdded = this.conversationService.addMessageToConversation(conversations.get().get(0), this.currentUser, instantMessageRequest);
-        if (!messageAdded) {
-            return;
-        }
+        System.out.println("Message added: " + messageAdded);
+//        if (!messageAdded) {
+//            return;
+//        }
         this.simpMessagingTemplate.convertAndSend("/message/receiver/" + instantMessageRequest.getReceiverId(), instantMessageRequest.getMessage());
     }
 }
