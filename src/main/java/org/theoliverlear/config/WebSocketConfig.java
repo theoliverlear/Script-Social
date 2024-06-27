8cc205725864635e41e96469a4af08e975e9b07c
theoliverlear <sigwartholiver2@gmail.com>
2024-06-27 15:13:19 -0500

    Refactor WebSocketConfig.java to handle session attributes and modify application destination prefixes

--- src/main/java/org/theoliverlear/config/WebSocketConfig.java
+++ src/main/java/org/theoliverlear/config/WebSocketConfig.java
@@ -1,21 +1,37 @@
 package org.theoliverlear.config;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").setHandshakeHandler(new DefaultHandshakeHandler() {
+            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+                if (request instanceof ServletServerHttpRequest servletRequest) {
+                    HttpSession session = servletRequest.getServletRequest().getSession();
+                    attributes.put("session", session);
+                }
+                return true;
+            }
+        }).withSockJS();
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/message/receiver");
-        config.setApplicationDestinationPrefixes("/message/send");
+        config.setApplicationDestinationPrefixes("/message");
     }
 }
