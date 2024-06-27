931abcb35e6ba0e71d60a6f966e07af7a33c5cf2
theoliverlear <sigwartholiver2@gmail.com>
2024-06-27 15:11:46 -0500

    Add methods for managing user websocket sessions in ScriptSocialService.java

--- src/main/java/org/theoliverlear/service/ScriptSocialService.java
+++ src/main/java/org/theoliverlear/service/ScriptSocialService.java
@@ -1,9 +1,12 @@
 package org.theoliverlear.service;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketSession;
 import org.theoliverlear.entity.user.User;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -18,5 +21,28 @@ public class ScriptSocialService {
         } else {
             return Optional.of(user);
         }
+    }
+    public Optional<User> getUserFromSession(SimpMessageHeaderAccessor headerAccessor) {
+        if (headerAccessor.getSessionAttributes() == null) {
+            return Optional.empty();
+        }
+        Map<String, Object> attributes = headerAccessor.getSessionAttributes();
+        return this.getUserFromSession((HttpSession) attributes.get("session"));
+    }
+    public Optional<HttpSession> getSessionFromAttributes(SimpMessageHeaderAccessor headerAccessor) {
+        if (headerAccessor.getSessionAttributes() == null) {
+            return Optional.empty();
+        }
+        Map<String, Object> attributes = headerAccessor.getSessionAttributes();
+        if (attributes.containsKey("session")) {
+            HttpSession session = (HttpSession) attributes.get("session");
+            return Optional.of(session);
+        } else {
+            return Optional.empty();
+        }
+    }
+    public boolean userInSession(SimpMessageHeaderAccessor headerAccessor) {
+        Optional<HttpSession> possibleSession = this.getSessionFromAttributes(headerAccessor);
+        return possibleSession.filter(this::userInSession).isPresent();
     }
 }
