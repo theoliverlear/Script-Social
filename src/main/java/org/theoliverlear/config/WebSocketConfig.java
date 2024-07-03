6b9e54178cf98269638d8e9ea0b201a5cc323652
theoliverlear <sigwartholiver2@gmail.com>
2024-07-03 14:07:36 -0500

    Refactor message broker url mapping in WebSocketConfig.java

--- src/main/java/org/theoliverlear/config/WebSocketConfig.java
+++ src/main/java/org/theoliverlear/config/WebSocketConfig.java
@@ -31,7 +31,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/message/receiver");
-        config.setApplicationDestinationPrefixes("/message");
+        config.enableSimpleBroker("/messages/receiver");
+        config.setApplicationDestinationPrefixes("/messages");
     }
 }
