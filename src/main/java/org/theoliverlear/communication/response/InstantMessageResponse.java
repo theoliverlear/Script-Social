771f7323d90627512db1da812d5beb3776655b5d
theoliverlear <sigwartholiver2@gmail.com>
2024-07-03 14:12:39 -0500

    Add userId field to InstantMessageResponse.java for more user identification information

--- src/main/java/org/theoliverlear/communication/response/InstantMessageResponse.java
+++ src/main/java/org/theoliverlear/communication/response/InstantMessageResponse.java
@@ -5,10 +5,12 @@ import lombok.Data;
 @Data
 public class InstantMessageResponse {
     private String fullNameOrUsername;
+    private Long userId;
     private String message;
     private String dateSent;
-    public InstantMessageResponse(String fullNameOrUsername, String message, String dateSent) {
+    public InstantMessageResponse(String fullNameOrUsername, Long userId, String message, String dateSent) {
         this.fullNameOrUsername = fullNameOrUsername;
+        this.userId = userId;
         this.message = message;
         this.dateSent = dateSent;
     }
