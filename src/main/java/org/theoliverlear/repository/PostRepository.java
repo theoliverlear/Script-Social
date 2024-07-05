7eddff57dc24357878bed66b399b37b79ea7e0b6
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:54:06 -0500

    Add comment headers for better code organization to PostRepository.java

--- src/main/java/org/theoliverlear/repository/PostRepository.java
+++ src/main/java/org/theoliverlear/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package org.theoliverlear.repository;
-
+//=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.content.Post;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    //=============================-Methods-==================================
+
+    //-----------------------Find-All-By-Poster-Id----------------------------
     List<Post> findAllByPosterId(Long userId);
 }
