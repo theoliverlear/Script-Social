ff7b0974406c395a19dd019dc67e3d35cfa44cd9
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:26:32 -0500

    Add comment headers for better code organization to AuthResponse.java

--- src/main/java/org/theoliverlear/communication/response/AuthResponse.java
+++ src/main/java/org/theoliverlear/communication/response/AuthResponse.java
@@ -1,11 +1,13 @@
 package org.theoliverlear.communication.response;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class AuthResponse {
+    //============================-Variables-=================================
     private boolean isAuthorized;
     private boolean completedWelcome;
+    //===========================-Constructors-===============================
     public AuthResponse(boolean isAuthorized, boolean completedWelcome) {
         this.isAuthorized = isAuthorized;
         this.completedWelcome = completedWelcome;
