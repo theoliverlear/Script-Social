e1c299d82e06e62051d48454fee4bad6a6a08cb8
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:48:51 -0500

    Add comment headers for better code organization to MessageRepository.java

--- src/main/java/org/theoliverlear/repository/MessageRepository.java
+++ src/main/java/org/theoliverlear/repository/MessageRepository.java
@@ -1,8 +1,9 @@
 package org.theoliverlear.repository;
-
+//=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.im.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    //=============================-Methods-==================================
 
 }
