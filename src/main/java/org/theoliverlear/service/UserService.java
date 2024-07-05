1c62d218bcb3326681cd4f6cbf4a0d1f25e09f6b
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:10:59 -0500

    Add comment headers for better code organization to UserService.java

--- src/main/java/org/theoliverlear/service/UserService.java
+++ src/main/java/org/theoliverlear/service/UserService.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.service;
-
+//=================================-Imports-==================================
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,35 +11,48 @@ import java.util.Optional;
 
 @Service
 public class UserService {
+    //============================-Variables-=================================
     private UserRepository userRepository;
+    //===========================-Constructors-===============================
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    //=============================-Methods-==================================
+
+    //-----------------------------Save-User----------------------------------
     public void saveUser(User user) {
         this.userRepository.save(user);
     }
+    //----------------------User-Exists-By-Username---------------------------
     public boolean userExistsByUsername(String username) {
-        return userRepository.existsByUsername(username);
+        return this.userRepository.existsByUsername(username);
     }
+    //-------------------------User-Exists-By-Id------------------------------
     public boolean userExistsById(Long id) {
         return this.userRepository.existsById(id);
     }
+    //------------------------Get-Current-User-Id-----------------------------
     public Long getCurrentUserId(HttpSession session) {
         User user = (User) session.getAttribute("user");
         if (user == null) {
             return null;
+        } else {
+            return user.getId();
         }
-        return user.getId();
     }
+    //--------------------------Find-By-Username------------------------------
     @Transactional
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
+    //------------------------Get-Current-Username----------------------------
     public String getCurrentUsername(HttpSession session) {
+        // TODO: Replace with Optional<String>.
         User user = (User) session.getAttribute("user");
         return user.getUsername();
     }
+    //---------------------------Get-User-By-Id-------------------------------
     @Transactional
     public Optional<User> getUserById(Long id) {
         User user = this.userRepository.findUserById(id);
