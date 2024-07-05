42f0179a0a9a9203cbeb5c2c3a7c31b8984938f1
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:35:19 -0500

    Add comment headers for better code organization to CommentRequest.java

--- src/main/java/org/theoliverlear/communication/request/CommentRequest.java
+++ src/main/java/org/theoliverlear/communication/request/CommentRequest.java
@@ -1,12 +1,14 @@
 package org.theoliverlear.communication.request;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class CommentRequest {
+    //============================-Variables-=================================
     private Long userId;
     private Long postId;
     private String content;
+    //===========================-Constructors-===============================
     public CommentRequest(Long userId, Long postId, String content) {
         this.userId = userId;
         this.postId = postId;
