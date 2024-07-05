9d1dcdb24231376345efdf2ec633439ca06ac5e5
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:47:27 -0500

    Add comment headers for better code organization to LogoutResponse.java

--- src/main/java/org/theoliverlear/communication/response/LogoutResponse.java
+++ src/main/java/org/theoliverlear/communication/response/LogoutResponse.java
@@ -1,10 +1,12 @@
 package org.theoliverlear.communication.response;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class LogoutResponse {
+    //============================-Variables-=================================
     private boolean isLoggedOut;
+    //===========================-Constructors-===============================
     public LogoutResponse(boolean isLoggedOut) {
         this.isLoggedOut = isLoggedOut;
     }
