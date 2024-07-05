43b91c8669d50273043622dd5a530777ceb75b24
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:56:33 -0500

    Add comment headers for better code organization to ProfileController.java

--- src/main/java/org/theoliverlear/controller/ProfileController.java
+++ src/main/java/org/theoliverlear/controller/ProfileController.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.controller;
-
+//=================================-Imports-==================================
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,11 +20,16 @@ import org.theoliverlear.service.ProfilePictureService;
 @RequestMapping("/profile")
 @Controller
 public class ProfileController {
+    //============================-Variables-=================================
     private ProfilePictureService profilePictureService;
+    //===========================-Constructors-===============================
     @Autowired
     public ProfileController(ProfilePictureService profilePictureService) {
         this.profilePictureService = profilePictureService;
     }
+    //=============================-Methods-==================================
+
+    //------------------------------Profile-----------------------------------
     @RequestMapping("/")
     public String profile(HttpSession session) {
         if (session.getAttribute("user") == null) {
@@ -32,6 +37,7 @@ public class ProfileController {
         }
         return "profile";
     }
+    //-----------------------------Profile-Id---------------------------------
     @RequestMapping("/{id}")
     public String profileId(HttpSession session, @PathVariable String id, Model model) {
         User sessionUser = (User) session.getAttribute("user");
@@ -41,6 +47,7 @@ public class ProfileController {
         model.addAttribute("userId", id);
         return "profile";
     }
+    //----------------------------Get-Profile---------------------------------
     @RequestMapping("/get/{id}")
     public ResponseEntity<ProfileResponse> getProfile(@PathVariable String id, HttpSession session, Model model) {
         User sessionUser = (User) session.getAttribute("user");
@@ -59,11 +66,13 @@ public class ProfileController {
                                                      "Hello, World!", false));
         }
     }
+    //------------------------Has-Profile-Picture-----------------------------
     @RequestMapping("/get/{id}/has-profile-picture")
     public ResponseEntity<HasProfilePictureResponse> hasProfilePicture(@PathVariable String id) {
         boolean hasProfilePicture = this.profilePictureService.existsByUserId(Long.parseLong(id));
         return ResponseEntity.ok(new HasProfilePictureResponse(hasProfilePicture));
     }
+    //------------------------Get-Profile-Picture-----------------------------
     @Transactional
     @RequestMapping("/get/{id}/profile-picture")
     public ResponseEntity<byte[]> getProfilePicture(@PathVariable String id) {
