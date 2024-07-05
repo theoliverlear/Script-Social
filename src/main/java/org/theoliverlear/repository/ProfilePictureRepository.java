4506f3d9fd9a126636d465cb0d296fb40b81ec3d
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:59:18 -0500

    Add comment headers for better code organization to ProfilePictureRepository.java

--- src/main/java/org/theoliverlear/repository/ProfilePictureRepository.java
+++ src/main/java/org/theoliverlear/repository/ProfilePictureRepository.java
@@ -1,9 +1,13 @@
 package org.theoliverlear.repository;
-
+//=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.user.ProfilePicture;
 
 public interface ProfilePictureRepository extends JpaRepository<ProfilePicture, Long> {
+    //=============================-Methods-==================================
+
+    //--------------------------Find-By-User-Id-------------------------------
     ProfilePicture findByUserId(Long userId);
+    //-------------------------Exists-By-User-Id------------------------------
     boolean existsByUserId(Long userId);
 }
