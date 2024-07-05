c86bfb95e0a24001985b38bdf74fc1003294ac1e
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:26:05 -0500

    Add comment headers for better code organization to AuthRequest.java

--- src/main/java/org/theoliverlear/communication/request/AuthRequest.java
+++ src/main/java/org/theoliverlear/communication/request/AuthRequest.java
@@ -1,11 +1,13 @@
 package org.theoliverlear.communication.request;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class AuthRequest {
+    //============================-Variables-=================================
     private String username;
     private String password;
+    //===========================-Constructors-===============================
     public AuthRequest(String username, String password) {
         this.username = username;
         this.password = password;
