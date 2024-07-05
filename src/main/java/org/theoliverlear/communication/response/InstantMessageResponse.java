2b1106313282350af7bb9c8cad650401b4d66885
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:44:03 -0500

    Add comment headers for better code organization to InstantMessageResponse.java

--- src/main/java/org/theoliverlear/communication/response/InstantMessageResponse.java
+++ src/main/java/org/theoliverlear/communication/response/InstantMessageResponse.java
@@ -1,13 +1,15 @@
 package org.theoliverlear.communication.response;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class InstantMessageResponse {
+    //============================-Variables-=================================
     private String fullNameOrUsername;
     private Long userId;
     private String message;
     private String dateSent;
+    //===========================-Constructors-===============================
     public InstantMessageResponse(String fullNameOrUsername, Long userId, String message, String dateSent) {
         this.fullNameOrUsername = fullNameOrUsername;
         this.userId = userId;
