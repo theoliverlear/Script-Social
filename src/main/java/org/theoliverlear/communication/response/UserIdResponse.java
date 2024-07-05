888f5a3f66942d582e5f52c88aeb63e2e64a9dd8
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:07:40 -0500

    Add comment headers for better code organization to UserIdResponse.java

--- src/main/java/org/theoliverlear/communication/response/UserIdResponse.java
+++ src/main/java/org/theoliverlear/communication/response/UserIdResponse.java
@@ -1,10 +1,12 @@
 package org.theoliverlear.communication.response;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class UserIdResponse {
+    //============================-Variables-=================================
     private Long userId;
+    //===========================-Constructors-===============================
     public UserIdResponse(Long userId) {
         this.userId = userId;
     }
