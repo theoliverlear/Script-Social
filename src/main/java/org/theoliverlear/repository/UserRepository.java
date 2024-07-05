3c3edc6f30fdc3622baeb05ce9f473b1fa5939df
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:09:00 -0500

    Add comment headers for better code organization to UserRepository.java

--- src/main/java/org/theoliverlear/repository/UserRepository.java
+++ src/main/java/org/theoliverlear/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package org.theoliverlear.repository;
-
+//=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    //=============================-Methods-==================================
+
+    //-------------------------Exists-By-Username-----------------------------
     boolean existsByUsername(String username);
+    //--------------------------Find-By-Username------------------------------
     User findByUsername(String username);
+    //---------------------------Find-By-Email--------------------------------
     User findByEmail(String email);
+    //-------------------------Finder-User-By-Id------------------------------
     User findUserById(Long id);
 }
