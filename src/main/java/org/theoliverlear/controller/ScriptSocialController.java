fbe0eeb0be93f78c2f66f8184ba2a6292a1ff650
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:02:55 -0500

    Add comment headers for better code organization to ScriptSocialController.java

--- src/main/java/org/theoliverlear/controller/ScriptSocialController.java
+++ src/main/java/org/theoliverlear/controller/ScriptSocialController.java
@@ -1,10 +1,13 @@
 package org.theoliverlear.controller;
-
+//=================================-Imports-==================================
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ScriptSocialController {
+    //=============================-Methods-==================================
+
+    //--------------------------------Home------------------------------------
     @RequestMapping("/")
     public String home() {
         return "home";
