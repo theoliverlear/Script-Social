8f478957e53c880952abcf49d32c22b07cb89a07
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:05:20 -0500

    Add comment headers for better code organization to SignupRequest.java

--- src/main/java/org/theoliverlear/communication/request/SignupRequest.java
+++ src/main/java/org/theoliverlear/communication/request/SignupRequest.java
@@ -1,12 +1,14 @@
 package org.theoliverlear.communication.request;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class SignupRequest {
+    //============================-Variables-=================================
     private String email;
     private String username;
     private String password;
+    //===========================-Constructors-===============================
     public SignupRequest(String email, String username, String password) {
         this.email = email;
         this.username = username;
