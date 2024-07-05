6e27a657878556ad073b1f83c961f49d799c044d
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:02:34 -0500

    Add comment headers for better code organization to ScriptSocialApplication.java

--- src/main/java/org/theoliverlear/ScriptSocialApplication.java
+++ src/main/java/org/theoliverlear/ScriptSocialApplication.java
@@ -1,10 +1,11 @@
 package org.theoliverlear;
-
+//=================================-Imports-==================================
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScriptSocialApplication {
+    //===============================-Main-===================================
     public static void main(String[] args) {
         SpringApplication.run(ScriptSocialApplication.class, args);
     }
