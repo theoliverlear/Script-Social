bbe995daeacfec9a535d1edc3cab86c9b27d52f4
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:05:38 -0500

    Add comment headers for better code organization to TitleFormatter.java

--- src/main/java/org/theoliverlear/model/TitleFormatter.java
+++ src/main/java/org/theoliverlear/model/TitleFormatter.java
@@ -1,11 +1,15 @@
 package org.theoliverlear.model;
-
+//=================================-Imports-==================================
 public class TitleFormatter {
+    //=============================-Methods-==================================
+
+    //-------------------------Format-Title-Case------------------------------
     public static String formatTitleCase(String title) {
         title = title.toLowerCase();
         title = title.substring(0, 1).toUpperCase() + title.substring(1);
         return title;
     }
+    //-------------------------Format-Title-Cases-----------------------------
     public static String formatTitleCases(String title) {
         String[] titleWords = title.split(" ");
         StringBuilder formattedTitle = new StringBuilder();
