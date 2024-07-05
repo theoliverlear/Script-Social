75096e71b21299adc2fc62873049c722d72a3464
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:49:48 -0500

    Add comment headers for better code organization to MessageService.java

--- src/main/java/org/theoliverlear/service/MessageService.java
+++ src/main/java/org/theoliverlear/service/MessageService.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.service;
-
+//=================================-Imports-==================================
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.theoliverlear.entity.im.Message;
@@ -7,11 +7,16 @@ import org.theoliverlear.repository.MessageRepository;
 
 @Service
 public class MessageService {
+    //============================-Variables-=================================
     private MessageRepository messageRepository;
+    //===========================-Constructors-===============================
     @Autowired
     public MessageService(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
+    //=============================-Methods-==================================
+
+    //----------------------------Save-Message--------------------------------
     public void saveMessage(Message message) {
         this.messageRepository.save(message);
     }
