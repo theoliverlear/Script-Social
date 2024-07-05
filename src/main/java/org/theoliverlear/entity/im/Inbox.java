285b9262ea2b69a884cfb8ec833df648d4158e1c
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:42:41 -0500

    Add TODO comment and comment headers for better code organization to Inbox.java

--- src/main/java/org/theoliverlear/entity/im/Inbox.java
+++ src/main/java/org/theoliverlear/entity/im/Inbox.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.entity.im;
-
+//=================================-Imports-==================================
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
@@ -10,11 +10,15 @@ import org.theoliverlear.entity.user.User;
 @Getter
 @Setter
 public class Inbox {
+    //============================-Variables-=================================
     @Id
     private Long id;
     @MapsId
     @JoinColumn(name = "user_id")
     private User user;
+    // TODO: Reevaluate purpose of class either as a container for
+    //       conversations or a container for unread messages.
+    //===========================-Constructors-===============================
     public Inbox() {
         this.user = null;
     }
