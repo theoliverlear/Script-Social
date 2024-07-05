00583bd3d970c946f5e1b8b4841b0a5af51a44c8
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:06:59 -0500

    Add comment headers for better code organization to UserController.java

--- src/main/java/org/theoliverlear/controller/UserController.java
+++ src/main/java/org/theoliverlear/controller/UserController.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.controller;
-
+//=================================-Imports-==================================
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,21 +12,29 @@ import org.theoliverlear.service.UserService;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    //============================-Variables-=================================
     private UserService userService;
+    //===========================-Constructors-===============================
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
+    //=============================-Methods-==================================
+
+    //------------------------Get-Current-User-Id-----------------------------
     @RequestMapping("/get/current/id")
     public ResponseEntity<UserIdResponse> getCurrentUserId(HttpSession session) {
+        // TODO: Replace with ScriptSocialService method.
         Long userId = this.userService.getCurrentUserId(session);
         if (userId == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         return new ResponseEntity<>(new UserIdResponse(userId), HttpStatus.OK);
     }
+    //------------------------Get-Current-Username----------------------------
     @RequestMapping("/get/current/username")
     public ResponseEntity<String> getCurrentUsername(HttpSession session) {
+        // TODO: Replace with ScriptSocialService method.
         String username = this.userService.getCurrentUsername(session);
         if (username == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
