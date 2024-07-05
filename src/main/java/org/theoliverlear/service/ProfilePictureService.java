e56f551531f8d1116fb49be5b06a0442bfa0a1b3
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:00:28 -0500

    Add comment headers for better code organization to ProfilePictureService.java

--- src/main/java/org/theoliverlear/service/ProfilePictureService.java
+++ src/main/java/org/theoliverlear/service/ProfilePictureService.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.service;
-
+//=================================-Imports-==================================
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,18 +8,25 @@ import org.theoliverlear.repository.ProfilePictureRepository;
 
 @Service
 public class ProfilePictureService {
-    ProfilePictureRepository profilePictureRepository;
+    //============================-Variables-=================================
+    private ProfilePictureRepository profilePictureRepository;
+    //===========================-Constructors-===============================
     @Autowired
     public ProfilePictureService(ProfilePictureRepository profilePictureRepository) {
         this.profilePictureRepository = profilePictureRepository;
     }
+    //=============================-Methods-==================================
+
+    //------------------------Save-Profile-Picture----------------------------
     public void saveProfilePicture(ProfilePicture profilePicture) {
         this.profilePictureRepository.save(profilePicture);
     }
+    //--------------------------Find-By-User-Id-------------------------------
     @Transactional
     public ProfilePicture findByUserId(Long userId) {
         return this.profilePictureRepository.findByUserId(userId);
     }
+    //-------------------------Exists-By-User-Id------------------------------
     public boolean existsByUserId(Long userId) {
         return this.profilePictureRepository.existsByUserId(userId);
     }
