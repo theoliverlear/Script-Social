3041a3b8f6c6a1bb3b8befb96c8f9a7786cb934c
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:34:58 -0500

    Add comment headers for better code organization to Comment.ts

--- src/main/resources/static/script/models/Comment.ts
+++ src/main/resources/static/script/models/Comment.ts
@@ -1,10 +1,12 @@
 export class Comment {
+    //============================-Variables-=================================
     private _userId: number;
     private _postId: number;
     private _commentId: number;
     private _content: string;
     private _datePosted: Date;
     private _comments: Comment[];
+    //===========================-Constructors-===============================
     constructor(userId: number = 0,
                 postId: number = 0,
                 commentId: number = 0,
@@ -18,6 +20,23 @@ export class Comment {
         this._datePosted = datePosted;
         this._comments = comments;
     }
+    //=============================-Methods-==================================
+
+    //----------------------------Add-Comment---------------------------------
+    addComment(comment: Comment): void {
+        this._comments.push(comment);
+    }
+    //---------------------------Remove-Comment-------------------------------
+    removeComment(comment: Comment): void {
+        this._comments = this._comments.filter((currentComment: Comment): boolean => {
+            return !currentComment.equals(comment);
+        });
+    }
+    //-------------------------------Equals-----------------------------------
+    equals(comment: Comment): boolean {
+        return this._commentId === comment.commentId;
+    }
+    //=============================-Getters-==================================
     get userId(): number {
         return this._userId;
     }
@@ -36,6 +55,7 @@ export class Comment {
     get comments(): Comment[] {
         return this._comments;
     }
+    //=============================-Setters-==================================
     setUserId(userId: number): void {
         this._userId = userId;
     }
@@ -53,16 +73,5 @@ export class Comment {
     }
     setComments(comments: Comment[]): void {
         this._comments = comments;
-    }
-    addComment(comment: Comment): void {
-        this._comments.push(comment);
-    }
-    removeComment(comment: Comment): void {
-        this._comments = this._comments.filter((currentComment: Comment): boolean => {
-            return !currentComment.equals(comment);
-        });
-    }
-    equals(comment: Comment): boolean {
-        return this._commentId === comment.commentId;
     }
 }
