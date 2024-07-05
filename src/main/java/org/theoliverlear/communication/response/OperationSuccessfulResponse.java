8d6194ebd0238796478d2a0e74d6d455f59d0f9c
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:50:49 -0500

    Add comment headers for better code organization and fix always true logic error in OperationSuccessfulResponse.java

--- src/main/java/org/theoliverlear/communication/response/OperationSuccessfulResponse.java
+++ src/main/java/org/theoliverlear/communication/response/OperationSuccessfulResponse.java
@@ -1,11 +1,13 @@
 package org.theoliverlear.communication.response;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class OperationSuccessfulResponse {
+    //============================-Variables-=================================
     boolean successful;
+    //===========================-Constructors-===============================
     public OperationSuccessfulResponse(boolean successful) {
-        this.successful = true;
+        this.successful = successful;
     }
 }
