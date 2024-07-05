7ca262166de8950bdd662d3ea21fe02bc825a509
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:41:45 -0500

    Add comment headers for better code organization to HasProfilePictureResponse.java

--- src/main/java/org/theoliverlear/communication/response/HasProfilePictureResponse.java
+++ src/main/java/org/theoliverlear/communication/response/HasProfilePictureResponse.java
@@ -1,10 +1,12 @@
 package org.theoliverlear.communication.response;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class HasProfilePictureResponse {
+    //============================-Variables-=================================
     boolean hasProfilePicture;
+    //===========================-Constructors-===============================
     public HasProfilePictureResponse(boolean hasProfilePicture) {
         this.hasProfilePicture = hasProfilePicture;
     }
