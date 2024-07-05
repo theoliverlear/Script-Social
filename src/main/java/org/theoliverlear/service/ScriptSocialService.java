ad0fa8d85ae786ddc6ee569d46b003411f343875
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:03:30 -0500

    Add comment headers for better code organization to ScriptSocialService.java

--- src/main/java/org/theoliverlear/service/ScriptSocialService.java
+++ src/main/java/org/theoliverlear/service/ScriptSocialService.java
@@ -1,9 +1,8 @@
 package org.theoliverlear.service;
-
+//=================================-Imports-==================================
 import jakarta.servlet.http.HttpSession;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.WebSocketSession;
 import org.theoliverlear.entity.user.User;
 
 import java.util.Map;
@@ -11,9 +10,13 @@ import java.util.Optional;
 
 @Service
 public class ScriptSocialService {
+    //=============================-Methods-==================================
+
+    //--------------------------User-In-Session-------------------------------
     public boolean userInSession(HttpSession session) {
         return session.getAttribute("user") != null;
     }
+    //-----------------------Get-User-From-Session----------------------------
     public Optional<User> getUserFromSession(HttpSession session) {
         User user = (User) session.getAttribute("user");
         if (user == null) {
@@ -22,6 +25,7 @@ public class ScriptSocialService {
             return Optional.of(user);
         }
     }
+    //-----------------------Get-User-From-Session----------------------------
     public Optional<User> getUserFromSession(SimpMessageHeaderAccessor headerAccessor) {
         if (headerAccessor.getSessionAttributes() == null) {
             return Optional.empty();
@@ -29,6 +33,7 @@ public class ScriptSocialService {
         Map<String, Object> attributes = headerAccessor.getSessionAttributes();
         return this.getUserFromSession((HttpSession) attributes.get("session"));
     }
+    //--------------------Get-Session-From-Attributes-------------------------
     public Optional<HttpSession> getSessionFromAttributes(SimpMessageHeaderAccessor headerAccessor) {
         if (headerAccessor.getSessionAttributes() == null) {
             return Optional.empty();
@@ -41,6 +46,7 @@ public class ScriptSocialService {
             return Optional.empty();
         }
     }
+    //--------------------------User-In-Session-------------------------------
     public boolean userInSession(SimpMessageHeaderAccessor headerAccessor) {
         Optional<HttpSession> possibleSession = this.getSessionFromAttributes(headerAccessor);
         return possibleSession.filter(this::userInSession).isPresent();
