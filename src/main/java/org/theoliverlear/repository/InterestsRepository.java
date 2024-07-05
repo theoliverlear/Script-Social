e68b6dfe6f258bd71d8549b1a3463166071f43f7
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:46:14 -0500

    Add comment headers for better code organization to InterestsRepository.java

--- src/main/java/org/theoliverlear/repository/InterestsRepository.java
+++ src/main/java/org/theoliverlear/repository/InterestsRepository.java
@@ -1,8 +1,11 @@
 package org.theoliverlear.repository;
-
+//=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.user.personal.Interests;
 
 public interface InterestsRepository extends JpaRepository<Interests, Long> {
+    //=============================-Methods-==================================
+
+    //--------------------------Find-By-User-Id-------------------------------
     Interests findByUserId(Long userId);
 }
