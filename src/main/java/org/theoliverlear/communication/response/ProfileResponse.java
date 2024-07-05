4c4ec9bf7a0a16729633f0e967919a07dff4c9e5
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 18:00:48 -0500

    Add comment headers for better code organization to ProfileResponse.java

--- src/main/java/org/theoliverlear/communication/response/ProfileResponse.java
+++ src/main/java/org/theoliverlear/communication/response/ProfileResponse.java
@@ -1,13 +1,15 @@
 package org.theoliverlear.communication.response;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class ProfileResponse {
+    //============================-Variables-=================================
     private String firstName;
     private String lastName;
     private String bio;
     private boolean isMe;
+    //===========================-Constructors-===============================
     public ProfileResponse(String firstName, String lastName,
                            String bio, boolean isMe) {
         this.firstName = firstName;
