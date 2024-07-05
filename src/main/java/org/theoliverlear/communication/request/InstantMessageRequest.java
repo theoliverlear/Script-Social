6486bd601481c32d6067a016781caa694749dc60
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 17:43:24 -0500

    Add comment headers for better code organization to InstantMessageRequest.java

--- src/main/java/org/theoliverlear/communication/request/InstantMessageRequest.java
+++ src/main/java/org/theoliverlear/communication/request/InstantMessageRequest.java
@@ -1,11 +1,13 @@
 package org.theoliverlear.communication.request;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class InstantMessageRequest {
+    //============================-Variables-=================================
     private Long receiverId;
     private String message;
+    //===========================-Constructors-===============================
     public InstantMessageRequest(Long receiverId, String message) {
         this.receiverId = receiverId;
         this.message = message;
