b3be3720efe38dc2f8d3f613f8967e5d6eb9815b
theoliverlear <sigwartholiver2@gmail.com>
2024-07-06 13:25:56 -0500

    Add comment headers for better code organization to createScript.ts

--- src/main/resources/static/script/createScript.ts
+++ src/main/resources/static/script/createScript.ts
@@ -1,17 +1,28 @@
-import {getCurrentUserIdFromServer, loadPage} from "./globalScript";
+//=================================-Imports-==================================
+import {
+    clearInput,
+    getCurrentUserIdFromServer,
+    loadPage
+} from "./globalScript";
 import {sendPostToServer} from "./postScript";
+//================================-Variables-=================================
 
+//----------------------------------Buttons-----------------------------------
 let writePostButton: JQuery<HTMLElement> = $('#write-post-button-div');
+//-----------------------------------Inputs-----------------------------------
 let writePostTextArea: JQuery<HTMLElement> = $('#write-post-textarea');
+//=============================-Server-Functions-=============================
 
+//---------------------------------Make-Post----------------------------------
 async function makePost(): Promise<void> {
     let postMessage: string = writePostTextArea.val() as string;
     let userId: number = await getCurrentUserIdFromServer();
     sendPostToServer(userId, postMessage);
-    // TODO: Make globalScript function called clearInput.
-    writePostTextArea.val('');
+    clearInput(writePostTextArea);
 }
+//================================-Init-Load-=================================
 let shouldLoadPage: boolean = loadPage(document.body, 'create');
+//=============================-Event-Listeners-==============================
 if (shouldLoadPage) {
     writePostButton.on('click', makePost);
 }
