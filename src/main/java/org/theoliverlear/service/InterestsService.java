ccd273130543b75b3d884adb754905aeee4c2e01
theoliverlear <sigwartholiver2@gmail.com>
2024-08-21 14:25:32 -0500

    Implement DatabaseAccessible and add update method in InterestsService.java

--- src/main/java/org/theoliverlear/service/InterestsService.java
+++ src/main/java/org/theoliverlear/service/InterestsService.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.theoliverlear.entity.user.User;
 import org.theoliverlear.entity.user.personal.Interests;
+import org.theoliverlear.model.DatabaseAccessible;
 import org.theoliverlear.repository.InterestsRepository;
 
 @Service
-public class InterestsService {
+public class InterestsService implements DatabaseAccessible<Interests> {
     //============================-Variables-=================================
     private InterestsRepository interestsRepository;
     //===========================-Constructors-===============================
@@ -18,8 +19,11 @@ public class InterestsService {
     //=============================-Methods-==================================
 
     //---------------------------Save-Interests-------------------------------
-    public void saveInterests(Interests interests) {
+    public void save(Interests interests) {
         this.interestsRepository.save(interests);
+    }
+    public Interests update(Interests interests) {
+        return this.interestsRepository.save(interests);
     }
     //--------------------------Find-By-User-Id-------------------------------
     public Interests findByUserId(User user) {
