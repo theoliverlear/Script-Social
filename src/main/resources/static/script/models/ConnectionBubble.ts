ae920596653b91cc1c02a7b1f02557fe59fd619c
theoliverlear <sigwartholiver2@gmail.com>
2024-08-25 17:47:31 -0500

    Add ConnectionBubble.ts to represent and generate HTML for user connection bubble components

---
+++ src/main/resources/static/script/models/ConnectionBubble.ts
@@ -0,0 +1,34 @@
+import {HtmlGenerative} from "./HtmlGenerative";
+import {
+    getProfilePictureSrcByUsername,
+    getThymeleafImageSrc
+} from "../globalScript";
+
+export class ConnectionBubble implements HtmlGenerative {
+    private _username: string;
+    constructor(username: string) {
+        this._username = username;
+    }
+    async getHtmlString(): Promise<string> {
+        let profilePictureSrc: string = await getProfilePictureSrcByUsername(this._username);
+        let thymeleafImageSrc: string = getThymeleafImageSrc(profilePictureSrc);
+        return `
+            <div class="connection-img-div">
+                <img class="connection-img" src="${profilePictureSrc}" alt="Default Avatar"
+                     th:src="${thymeleafImageSrc}">
+            </div>
+            <div class="connection-name-div">
+                <h6 class="connection-name-text">${this._username}</h6>
+            </div>
+        `;
+    }
+    async getHtml(): Promise<HTMLDivElement> {
+        let connectionBubbleDiv: HTMLDivElement = document.createElement('div');
+        connectionBubbleDiv.classList.add('connection-item');
+        connectionBubbleDiv.innerHTML = await this.getHtmlString();
+        return connectionBubbleDiv;
+    }
+    get username(): string {
+        return this._username;
+    }
+}
