d9181029d87d4caf181e4fbe16900653b37f0815
theoliverlear <sigwartholiver2@gmail.com>
2024-09-05 14:41:59 -0500

    Add authorize.component.ts to handle authorization functionality in Angular project

---
+++ src/main/resources/static/script/angular/components/pages/authorize/authorize.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'authorize',
+    templateUrl: './authorize.component.html',
+    styleUrls: ['./authorize-style.component.css']
+})
+export class AuthorizeComponent {
+
+}
