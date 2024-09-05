f13b3938464a4f53be24962ad5b52edb363ae482
theoliverlear <sigwartholiver2@gmail.com>
2024-09-05 14:57:13 -0500

    Add polyfills.ts to handle compatibility with core-js and zone.js for Angular in the static script directory

---
+++ src/main/resources/static/script/angular/polyfills.ts
@@ -0,0 +1,14 @@
+import 'core-js/es/array';
+import 'core-js/es/function';
+import 'core-js/es/map';
+import 'core-js/es/math';
+import 'core-js/es/number';
+import 'core-js/es/object';
+import 'core-js/es/promise';
+import 'core-js/es/regexp';
+import 'core-js/es/set';
+import 'core-js/es/string';
+import 'core-js/es/symbol';
+import 'zone.js';  // Required for Angular
+
+// Add any custom polyfills below or any additional imports specific to your project requirements.
