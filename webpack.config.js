978f358003493a554bada0cf47e8cd60e47b9a1e
theoliverlear <sigwartholiver2@gmail.com>
2024-09-06 14:57:21 -0500

    Add 'window.jQuery' to webpack.ProvidePlugin in webpack.config.js to ensure global jQuery availability

--- webpack.config.js
+++ webpack.config.js
@@ -98,7 +98,8 @@ module.exports = {
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
-            jQuery: 'jquery'
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
         }),
         new AngularWebpackPlugin({
             tsconfig: './tsconfig.json',
