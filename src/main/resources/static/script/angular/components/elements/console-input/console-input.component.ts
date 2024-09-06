c1b167c0e35483d30e2d1aedf6087094166791be
theoliverlear <sigwartholiver2@gmail.com>
2024-09-06 14:55:48 -0500

    Add input handling and maxlength functionality to console-input.component.ts including AfterViewInit implementation

--- src/main/resources/static/script/angular/components/elements/console-input/console-input.component.ts
+++ src/main/resources/static/script/angular/components/elements/console-input/console-input.component.ts
@@ -1,4 +1,11 @@
-import {Component, Input} from "@angular/core";
+import {
+    AfterViewInit,
+    Component, ElementRef,
+    EventEmitter,
+    Input,
+    Output,
+    Renderer2
+} from "@angular/core";
 import {ConsoleInputType} from "./models/ConsoleInputType";
 import {ConsoleInputField} from "./models/ConsoleInputField";
 
@@ -7,10 +14,43 @@ import {ConsoleInputField} from "./models/ConsoleInputField";
     templateUrl: './console-input.component.html',
     styleUrls: ['./console-input-style.component.css']
 })
-export class ConsoleInputComponent {
+export class ConsoleInputComponent implements AfterViewInit {
     @Input() title: ConsoleInputField;
     @Input() type: ConsoleInputType;
-    constructor() {
+    @Input() value: string = '';
+    @Input() maxValue: number = 200;
+    @Output() valueChange: EventEmitter<string> = new EventEmitter();
+    private inputElement: HTMLInputElement | null = null;
+    constructor(private renderer: Renderer2, private element: ElementRef) {
         console.log('ConsoleInputComponent loaded');
     }
+    ngAfterViewInit() {
+        this.inputElement = this.element.nativeElement.querySelector('input');
+        if (this.inputElement) {
+            this.updateMaxLengthIfApplicable();
+        }
+    }
+    updateMaxLengthIfApplicable() {
+        if (this.shouldHaveMaxLength()) {
+            this.updateMaxLength();
+        } else {
+            this.renderer.removeAttribute(this.inputElement, 'maxlength');
+        }
+    }
+    updateMaxLength() {
+        this.renderer.setAttribute(this.inputElement, 'maxlength', this.maxValue.toString());
+    }
+    handleInputChange(newValue: string) {
+        this.value = newValue;
+        this.valueChange.emit(this.value);
+        console.log('Input changed: ', this.value);
+    }
+    shouldHaveMaxLength(): boolean {
+        return [
+            ConsoleInputType.TEXT,
+            ConsoleInputType.PASSWORD,
+            ConsoleInputType.EMAIL
+        ].includes(this.type);
+    }
+
 }
