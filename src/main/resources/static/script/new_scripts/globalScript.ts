fb7f70f92b7acbc2216830a00872dede491255e1
theoliverlear <sigwartholiver2@gmail.com>
2024-09-08 17:11:01 -0500

    Add sendLoginRequest and getCurrentUserIdFromServer methods to handle user authentication in globalScript.ts

--- src/main/resources/static/script/new_scripts/globalScript.ts
+++ src/main/resources/static/script/new_scripts/globalScript.ts
@@ -22,7 +22,39 @@ export function sendSignupToServer(username: string, email: string, unwashedPass
     });
     return isAuthorized;
 }
+//-----------------------------Send-Login-Request-----------------------------
+export function sendLoginRequest(username: string, password: string): boolean {
+    let hashedPassword: string = hashPassword(password);
+    let isAuthorized: boolean = false;
+    fetch('/authorize/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            password: hashedPassword
+        })
+    }).then((response: Response) => {
+        return response.json();
+    }).then(responseJson =>{
+        isAuthorized = responseJson.authorized;
+    }).catch((error): void => {
+        console.error('Error: ', error);
+    });
+    return isAuthorized;
+}
 export function hashPassword(password: string): string {
     let hashedPassword: CryptoJS.lib.WordArray = CryptoJS.SHA256(password);
     return hashedPassword.toString();
+}
+//----------------------Get-Current-User-Id-From-Server-----------------------
+export async function getCurrentUserIdFromServer(): Promise<number> {
+    let response: Response = await fetch('/user/get/current/id');
+    if (response.ok) {
+        let responseJson: any = await response.json();
+        return responseJson.userId;
+    } else {
+        return -1;
+    }
 }
