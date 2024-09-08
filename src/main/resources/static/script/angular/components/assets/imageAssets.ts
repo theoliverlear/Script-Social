b28f41fe8d3fa902422093dda3c6f03c0989b156
theoliverlear <sigwartholiver2@gmail.com>
2024-09-08 18:36:45 -0500

    Add imageAssets.ts to manage paths and alt text for images and icons

---
+++ src/main/resources/static/script/angular/components/assets/imageAssets.ts
@@ -0,0 +1,23 @@
+export type ImageAsset = {
+    src: string;
+    alt: string;
+}
+const imageAssetPath: string = 'assets/image/';
+const logoImageAssetPath: string = imageAssetPath + 'logo/';
+const iconImageAssetPath: string = imageAssetPath + 'icon/';
+export const scriptSocialLogoCropped: ImageAsset = {
+    src: logoImageAssetPath + 'script_social_cropped.png',
+    alt: 'Script Social Logo'
+}
+export const hamburgerIcon: ImageAsset = {
+    src: iconImageAssetPath + 'hamburger_menu.svg',
+    alt: 'Hamburger Menu'
+}
+export const searchIcon: ImageAsset = {
+    src: iconImageAssetPath + 'search_icon.png',
+    alt: 'Search Icon'
+}
+export const defaultAvatar: ImageAsset = {
+    src: iconImageAssetPath + 'default_avatar.svg',
+    alt: 'User Avatar'
+}
