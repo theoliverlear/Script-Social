74582ac06940e9fffbb531278a81736e58cff4e4
theoliverlear <sigwartholiver2@gmail.com>
2024-09-08 18:43:53 -0500

    Add TypeSpeed.ts to define typing speed constants in Angular model

---
+++ src/main/resources/static/script/angular/components/elements/models/TypeSpeed.ts
@@ -0,0 +1,7 @@
+export enum TypeSpeed {
+    // TODO: Test this, it probably is in reverse order currently. Higher
+    //       number for a higher timeout would indicate this is in reverse.
+    SLOW = 100,
+    NORMAL = 150,
+    FAST = 200
+}
