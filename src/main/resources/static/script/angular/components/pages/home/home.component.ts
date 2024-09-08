6dfd6fd6644f1e3d11ebb74053bbad3b96e83838
theoliverlear <sigwartholiver2@gmail.com>
2024-09-08 13:35:05 -0500

    Add subscription handlers and implement OnInit lifecycle in HomeComponent in home.component.ts

--- src/main/resources/static/script/angular/components/pages/home/home.component.ts
+++ src/main/resources/static/script/angular/components/pages/home/home.component.ts
@@ -1,19 +1,41 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {ConsoleType} from "../../elements/console/models/ConsoleType";
 import {TagType} from "../../elements/models/TagType";
+import {AuthPopup} from "../../elements/models/auth/AuthPopup";
+import {SignupHandlerService} from "../../../services/signup-handler.service";
+import {
+    PasswordMatchHandlerService
+} from "../../../services/password-match-handler.service";
+import {
+    EmailValidatorService
+} from "../../../services/email-validator.service";
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home-style.component.css']
 })
-export class HomeComponent {
-    title: string = 'Home | Script Social'
+export class HomeComponent implements OnInit {
+    title: string = 'Home | Script Social';
+    authPopup: AuthPopup;
     stylesheets: string[] = [
 
-        ]
-    constructor() {
+        ];
+    constructor(private signupHandlerService: SignupHandlerService,
+                private passwordMatchHandlerService: PasswordMatchHandlerService,
+                private emailValidatorService: EmailValidatorService) {
         console.log('HomeComponent loaded');
+    }
+    ngOnInit() {
+        this.signupHandlerService.signupFailed$.subscribe((authPopup: AuthPopup): void => {
+            this.authPopup = authPopup;
+        });
+        this.passwordMatchHandlerService.passwordMismatch$.subscribe((isMismatch: boolean): void => {
+            this.authPopup = isMismatch ? AuthPopup.PASSWORDS_DONT_MATCH : null;
+        });
+        this.emailValidatorService.validEmail$.subscribe((authPopup: AuthPopup): void => {
+            this.authPopup = authPopup;
+        });
     }
 
     protected readonly ConsoleType = ConsoleType;
