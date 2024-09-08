1d78ec89ebc01e3846038d716cce47d3064d94d5
theoliverlear <sigwartholiver2@gmail.com>
2024-09-08 17:11:43 -0500

    Remove unused services and redundant code in home.component.ts as functionality has consolidated

--- src/main/resources/static/script/angular/components/pages/home/home.component.ts
+++ src/main/resources/static/script/angular/components/pages/home/home.component.ts
@@ -1,14 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {ConsoleType} from "../../elements/console/models/ConsoleType";
 import {TagType} from "../../elements/models/TagType";
-import {AuthPopup} from "../../elements/models/auth/AuthPopup";
-import {SignupHandlerService} from "../../../services/signup-handler.service";
-import {
-    PasswordMatchHandlerService
-} from "../../../services/password-match-handler.service";
-import {
-    EmailValidatorService
-} from "../../../services/email-validator.service";
 
 @Component({
     selector: 'home',
@@ -17,25 +9,15 @@ import {
 })
 export class HomeComponent implements OnInit {
     title: string = 'Home | Script Social';
-    authPopup: AuthPopup;
+
     stylesheets: string[] = [
 
         ];
-    constructor(private signupHandlerService: SignupHandlerService,
-                private passwordMatchHandlerService: PasswordMatchHandlerService,
-                private emailValidatorService: EmailValidatorService) {
+    constructor() {
         console.log('HomeComponent loaded');
     }
     ngOnInit() {
-        this.signupHandlerService.signupFailed$.subscribe((authPopup: AuthPopup): void => {
-            this.authPopup = authPopup;
-        });
-        this.passwordMatchHandlerService.passwordMismatch$.subscribe((isMismatch: boolean): void => {
-            this.authPopup = isMismatch ? AuthPopup.PASSWORDS_DONT_MATCH : null;
-        });
-        this.emailValidatorService.validEmail$.subscribe((authPopup: AuthPopup): void => {
-            this.authPopup = authPopup;
-        });
+
     }
 
     protected readonly ConsoleType = ConsoleType;
