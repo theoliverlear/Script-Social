d97655a1bfd2aaaea59ca82918cad5f4e7e727a0
theoliverlear <sigwartholiver2@gmail.com>
2024-09-08 13:27:30 -0500

    Add AuthPopup.ts to define authentication popup messages

---
+++ src/main/resources/static/script/angular/components/elements/models/auth/AuthPopup.ts
@@ -0,0 +1,9 @@
+export enum AuthPopup {
+    //============================-Constants-=================================
+    PASSWORDS_DONT_MATCH = 'Passwords do not match.',
+    INVALID_EMAIL = 'Invalid email.',
+    USERNAME_OR_EMAIL_EXISTS = 'Username or email already exists',
+    FILL_ALL_FIELDS = 'Please fill all fields.',
+    INVALID_USERNAME_OR_PASSWORD = 'Invalid username or password.',
+    NONE = '',
+}
