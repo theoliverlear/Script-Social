0728de577e7046286f306ce865a9ed267c020763
theoliverlear <sigwartholiver2@gmail.com>
2024-09-09 17:28:14 -0500

    Add AuthBubbleComponent to handle authentication types in auth-bubble.component.ts

---
+++ src/main/resources/static/script/angular/components/elements/auth-bubble/auth-bubble.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from "@angular/core";
+import {AuthType} from "../models/auth/AuthType";
+
+@Component({
+    selector: 'auth-bubble',
+    templateUrl: './auth-bubble.component.html',
+    styleUrls: ['./auth-bubble-style.component.css']
+})
+export class AuthBubbleComponent {
+    @Input() authType: AuthType;
+
+    constructor() {
+        console.log('AuthBubbleComponent loaded');
+    }
+}
