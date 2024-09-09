2c9ba5d1978a50d26494f11fbc7052ae31aa9c47
theoliverlear <sigwartholiver2@gmail.com>
2024-09-09 17:35:52 -0500

    Correct timings in TypeSpeed.ts and remove the corresponding TODO comment

--- src/main/resources/static/script/angular/components/elements/models/TypeSpeed.ts
+++ src/main/resources/static/script/angular/components/elements/models/TypeSpeed.ts
@@ -1,7 +1,5 @@
 export enum TypeSpeed {
-    // TODO: Test this, it probably is in reverse order currently. Higher
-    //       number for a higher timeout would indicate this is in reverse.
-    SLOW = 100,
+    SLOW = 200,
     NORMAL = 150,
-    FAST = 200
+    FAST = 100
 }
