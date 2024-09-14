2a849caedfaed85f5d89c26fe04c65444301a182
theoliverlear <sigwartholiver2@gmail.com>
2024-09-14 16:13:56 -0500

    Add agree-terms.service.ts to handle user agreement to terms logic in an Angular service

---
+++ src/main/resources/static/script/angular/services/agree-terms.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@angular/core";
+import {Subject} from "rxjs";
+import {AuthPopup} from "../components/elements/models/auth/AuthPopup";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AgreeTermsService {
+    private agreeTermsSubject: Subject<AuthPopup> = new Subject();
+    termsAgreed$ = this.agreeTermsSubject.asObservable();
+    constructor() {
+        console.log('AgreeTermsService loaded');
+    }
+    termsAgreed(terms: boolean): boolean {
+        return terms;
+    }
+    emitAgreeTerms(authPopup: AuthPopup) {
+        this.agreeTermsSubject.next(authPopup);
+    }
+}
