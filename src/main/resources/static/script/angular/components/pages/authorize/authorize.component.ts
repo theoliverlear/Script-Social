68f541456f522a9974ca7b6b0b1df626b176769f
theoliverlear <sigwartholiver2@gmail.com>
2024-09-15 15:58:10 -0500

    Add ViewChild and AuthType imports and implement auth type change handling in authorize.component.ts

--- src/main/resources/static/script/angular/components/pages/authorize/authorize.component.ts
+++ src/main/resources/static/script/angular/components/pages/authorize/authorize.component.ts
@@ -1,4 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
+import {ConsoleType} from "../../elements/console/models/ConsoleType";
+import {AuthType} from "../../elements/models/auth/AuthType";
+import {ConsoleComponent} from "../../elements/console/console.component";
 
 @Component({
     selector: 'authorize',
@@ -6,5 +9,18 @@ import {Component} from "@angular/core";
     styleUrls: ['./authorize-style.component.css']
 })
 export class AuthorizeComponent {
-
+    selectedAuthType: AuthType | null = AuthType.SIGNUP;
+    @ViewChild(ConsoleComponent) consoleComponent: ConsoleComponent;
+    constructor() {
+        console.log('AuthorizeComponent loaded');
+    }
+    get consoleType(): ConsoleType {
+        return this.selectedAuthType === AuthType.SIGNUP ? ConsoleType.SIGNUP : ConsoleType.LOGIN;
+    }
+    onAuthTypeChange(authType: AuthType): void {
+        this.selectedAuthType = authType;
+        if (this.consoleComponent) {
+            this.consoleComponent.clear();
+        }
+    }
 }
