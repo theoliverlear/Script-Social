fca9f0f5851712f4e2335637788eb66abb7db765
theoliverlear <sigwartholiver2@gmail.com>
2024-10-11 17:03:45 -0500

    Add ProfileNameComponent in profile-name.component.ts to display user's profile name

---
+++ src/main/resources/static/script/angular/components/elements/profile-name/profile-name.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from "@angular/core";
+
+@Component({
+    selector: 'profile-name',
+    templateUrl: './profile-name.component.html',
+    styleUrls: ['./profile-name-style.component.css']
+})
+export class ProfileNameComponent {
+    @Input() nameText: string;
+    constructor() {
+        console.log('ProfileNameComponent loaded');
+    }
+}
