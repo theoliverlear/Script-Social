447af34c4bbfd8e4d6136c516fbd232470528152
theoliverlear <sigwartholiver2@gmail.com>
2024-10-11 16:40:36 -0500

    Update import path for AuthPopup in console-popup.component.ts for correct directory structure

--- src/main/resources/static/script/angular/components/elements/console-popup/console-popup.component.ts
+++ src/main/resources/static/script/angular/components/elements/console-popup/console-popup.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from "@angular/core";
-import {AuthPopup} from "../models/auth/AuthPopup";
+import {AuthPopup} from "../../models/auth/AuthPopup";
 
 @Component({
     selector: 'console-popup',
