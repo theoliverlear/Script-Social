68e46a9424c6de1e944da719d53271651c572317
theoliverlear <sigwartholiver2@gmail.com>
2024-10-11 17:28:46 -0500

    Add directives.ts to consolidate and export Angular directives for better modularity and organization

---
+++ src/main/resources/static/script/angular/directives/directives.ts
@@ -0,0 +1,29 @@
+import {AgreeTermsDirective} from "./agree-terms.directive";
+import {AutoPasswordMatchDirective} from "./auto-password-match.directive";
+import {AutoPopupDirective} from "./auto-popup.directive";
+import {AutoSignupConsoleDirective} from "./auto-signup-console.directive";
+import {EmailValidatorDirective} from "./email-validator.directive";
+import {
+    FilledFieldsHandlerDirective
+} from "./filled-fields-handler.directive";
+import {
+    PasswordMatchHandlerDirective
+} from "./password-match-handler.directive";
+import {
+    SignupConsoleHandlerDirective
+} from "./signup-console-handler.directive";
+import {UndraggableDirective} from "./undraggable.directive";
+import {UnoptimizedImageDirective} from "./unoptimized-image.directive";
+
+export const directives = [
+    AgreeTermsDirective,
+    AutoPasswordMatchDirective,
+    AutoPopupDirective,
+    AutoSignupConsoleDirective,
+    EmailValidatorDirective,
+    FilledFieldsHandlerDirective,
+    PasswordMatchHandlerDirective,
+    SignupConsoleHandlerDirective,
+    UndraggableDirective,
+    UnoptimizedImageDirective
+]
