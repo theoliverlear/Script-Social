fb3e841e92ee41ad7dcd13d5840407c1c9abec14
theoliverlear <sigwartholiver2@gmail.com>
2024-10-11 13:19:20 -0500

    Add ProfileComponent to route definitions in app-routing.module.ts

--- src/main/resources/static/script/angular/modules/app-routing.module.ts
+++ src/main/resources/static/script/angular/modules/app-routing.module.ts
@@ -1,9 +1,16 @@
-import {RouterModule, Routes} from "@angular/router";
+import {Route, RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {HomeComponent} from "../components/pages/home/home.component";
 import {
     AuthorizeComponent
 } from "../components/pages/authorize/authorize.component";
+import {
+    ProfileComponent
+} from "../components/pages/profile/profile.component";
+
+const homeRoute: Route = {
+
+}
 
 const routes: Routes = [
     {
@@ -22,11 +29,35 @@ const routes: Routes = [
             meta: {title: 'Authorize | Script Social',
                    stylesheets: []}
         }
-    }
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        data: {
+            meta: {
+                title: 'Profile | Script Social',
+                stylesheets: []
+            }
+        }
+    },
+    // {
+    //     path: 'profile/:id',
+    //     component: ProfileComponent,
+    //     data: {
+    //         meta: {
+    //             title: 'Profile | Script Social',
+    //             stylesheets: []
+    //         }
+    //     }
+    // }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRouting { }
+export class AppRouting {
+    constructor() {
+        console.log('AppRouting loaded');
+    }
+}
