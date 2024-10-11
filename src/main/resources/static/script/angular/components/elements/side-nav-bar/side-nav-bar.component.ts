a14601e7b56261045d0f063f0b16bc4e8dedb6f1
theoliverlear <sigwartholiver2@gmail.com>
2024-10-11 16:57:15 -0500

    Add slideInOut animations to SideNavBarComponent in side-nav-bar.component.ts

--- src/main/resources/static/script/angular/components/elements/side-nav-bar/side-nav-bar.component.ts
+++ src/main/resources/static/script/angular/components/elements/side-nav-bar/side-nav-bar.component.ts
@@ -11,22 +11,41 @@ import {
 import {SwipeService} from "../../../services/swipe.service";
 import {SwipeDirection} from "../../events/models/SwipeDirection";
 import {SwipeEvent} from "../../events/SwipeEvent";
+import {
+    slideInOutAnimation
+} from "../../animations/animations";
+import {
+    sideNavBarSlideAnimationProperties
+} from "../../animations/animationProperties";
 
 @Component({
     selector: 'side-nav-bar',
     templateUrl: './side-nav-bar.component.html',
-    styleUrls: ['./side-nav-bar-style.component.css']
+    styleUrls: ['./side-nav-bar-style.component.css'],
+    animations: [slideInOutAnimation]
 })
 export class SideNavBarComponent implements AfterViewInit {
-    sideNavBarExpanded = false;
+    sideNavBarExpanded: boolean = false;
     @HostBinding('class.side-nav-closed') isSideNavClosed = true;
+    @HostBinding('@slideInOut') get slideAnimation(): {
+        params: { openedWidth: string; closedWidth: string };
+        value: "opened" | "closed"
+    } {
+        return {
+            value: this.sideNavBarExpanded ? 'opened' : 'closed',
+            params: {
+                closedWidth: sideNavBarSlideAnimationProperties.closedWidth,
+                openedWidth: sideNavBarSlideAnimationProperties.openedWidth
+            }
+        }
+    }
     constructor(private element: ElementRef, private renderer: Renderer2, private swipeService: SwipeService) {
         console.log('SideNavBarComponent loaded');
     }
     ngAfterViewInit(): void {
         const sideNavBarElement = this.element.nativeElement;
         console.log('sideNavBarElement', sideNavBarElement);
-        this.swipeService.detectSwipe(sideNavBarElement, (event: SwipeEvent) => {
+        this.swipeService.detectSwipe(sideNavBarElement, (event: SwipeEvent): void => {
             console.log('swipe event', event);
             if (event.direction === SwipeDirection.LEFT && this.sideNavBarExpanded) {
                 this.toggleSideNavBarPopout();
@@ -35,7 +54,7 @@ export class SideNavBarComponent implements AfterViewInit {
             }
         });
     }
-    toggleSideNavBarPopout() {
+    toggleSideNavBarPopout(): void {
         this.sideNavBarExpanded = !this.sideNavBarExpanded;
         this.flipSideBarTabImage();
         this.toggleTextVisibility();
@@ -49,7 +68,7 @@ export class SideNavBarComponent implements AfterViewInit {
             this.renderer.addClass(sideNavBarElement, 'side-nav-closed');
         }
     }
-    toggleTextVisibility() {
+    toggleTextVisibility(): void {
         const sideNavBarText = this.element.nativeElement.querySelectorAll('.side-bar-icon-title-div');
         if (this.sideNavBarExpanded) {
             sideNavBarText.forEach((text: Element) => {
@@ -61,7 +80,7 @@ export class SideNavBarComponent implements AfterViewInit {
             });
         }
     }
-    flipSideBarTabImage() {
+    flipSideBarTabImage(): void {
         const sideBarTabImg = this.element.nativeElement.querySelector('#side-bar-tab-img');
         if (this.sideNavBarExpanded) {
             this.renderer.setStyle(sideBarTabImg, 'transform', 'rotate(180deg)');
