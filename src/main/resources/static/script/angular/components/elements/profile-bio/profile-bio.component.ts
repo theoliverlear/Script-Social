dfc029f03d4fdd3f77d97de4cefc8c247bddddfb
theoliverlear <sigwartholiver2@gmail.com>
2024-10-11 17:06:24 -0500

    Add profile-bio.component.ts to manage user bio display in profile section

---
+++ src/main/resources/static/script/angular/components/elements/profile-bio/profile-bio.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from "@angular/core";
+
+@Component({
+    selector: 'profile-bio',
+    templateUrl: './profile-bio.component.html',
+    styleUrls: ['./profile-bio-style.component.css']
+})
+export class ProfileBioComponent {
+    @Input() bioText: string;
+    constructor() {
+        console.log('ProfileBioComponent loaded');
+    }
+    hasBioText(): boolean {
+        return this.bioText !== undefined && this.bioText !== '';
+    }
+}
