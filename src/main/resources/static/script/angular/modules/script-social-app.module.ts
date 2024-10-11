81198433be82a537316151a71d2a70b55b636d94
theoliverlear <sigwartholiver2@gmail.com>
2024-10-11 17:27:26 -0500

    Refactor module imports and declarations to use spread arrays for more clear and concise code in script-social-app.module.ts

--- src/main/resources/static/script/angular/modules/script-social-app.module.ts
+++ src/main/resources/static/script/angular/modules/script-social-app.module.ts
@@ -1,166 +1,40 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import { AppComponent } from '../components/app/app.component';
-import { NavBarComponent } from '../components/elements/nav-bar/nav-bar.component';
 import {NgOptimizedImage} from "@angular/common";
-import { SsFooterComponent } from '../components/elements/ss-footer/ss-footer.component';
 import {FormsModule} from "@angular/forms";
 import { AppRouting } from './app-routing.module';
 import {RouterOutlet} from "@angular/router";
-import {HomeComponent} from "../components/pages/home/home.component";
-import {
-    HeadComponent
-} from "../components/elements/ss-head/ss-head.component";
-import {
-    UnoptimizedImageDirective
-} from "../directives/unoptimized-image.directive";
-import {
-    ConsoleComponent
-} from "../components/elements/console/console.component";
-import {
-    AuthorizeComponent
-} from "../components/pages/authorize/authorize.component";
-import {
-    SsButtonComponent
-} from "../components/elements/ss-button/ss-button.component";
-import {
-    ConsoleInputComponent
-} from "../components/elements/console-input/console-input.component";
-import {UndraggableDirective} from "../directives/undraggable.directive";
-import {
-    ConsolePopupComponent
-} from "../components/elements/console-popup/console-popup.component";
-import {
-    TypableTextComponent
-} from "../components/elements/typable-text/typable-text.component";
-import {
-    BlinkingCursorComponent
-} from "../components/elements/blinking-cursor/blinking-cursor.component";
-import {SignupHandlerService} from "../services/signup-handler.service";
-import {
-    SignupConsoleHandlerDirective
-} from "../directives/signup-console-handler.directive";
-import {
-    AutoSignupConsoleDirective
-} from "../directives/auto-signup-console.directive";
-import {
-    PasswordMatchHandlerService
-} from "../services/password-match-handler.service";
-import {
-    PasswordMatchHandlerDirective
-} from "../directives/password-match-handler.directive";
-import {
-    AutoPasswordMatchDirective
-} from "../directives/auto-password-match.directive";
-import {AutoPopupDirective} from "../directives/auto-popup.directive";
-import {
-    EmailValidatorDirective
-} from "../directives/email-validator.directive";
-import {ConsoleService} from "../services/console.service";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {SsImgComponent} from "../components/elements/ss-img/ss-img.component";
-import {
-    AuthBubbleComponent
-} from "../components/elements/auth-bubble/auth-bubble.component";
-import {
-    AuthTypeSelectorComponent
-} from "../components/elements/auth-type-selector/auth-type-selector.component";
-import {ErrorHandlerService} from "../services/error-handler.service";
-import {HashPasswordService} from "../services/hash-password.service";
-import {SignupService} from "../services/signup.service";
 import {
     HttpClientModule,
     provideHttpClient,
     withFetch
 } from "@angular/common/http";
-import {
-    FilledFieldsHandlerDirective
-} from "../directives/filled-fields-handler.directive";
-import {FilledFieldsService} from "../services/filled-fields.service";
-import {AgreeTermsDirective} from "../directives/agree-terms.directive";
-import {EmailValidatorService} from "../services/email-validator.service";
-import {AgreeTermsService} from "../services/agree-terms.service";
-import {
-    ProfileComponent
-} from "../components/pages/profile/profile.component";
-import {
-    HeadlineButtonComponent
-} from "../components/elements/headline-button/headline-button.component";
-import {
-    ProfileHeadlineButtonsComponent
-} from "../components/elements/profile-headline-buttons/profile-headline-buttons.component";
-import {
-    SideNavBarComponent
-} from "../components/elements/side-nav-bar/side-nav-bar.component";
-import {
-    SideNavBarButtonComponent
-} from "../components/elements/side-nav-bar-button/side-nav-bar-button.component";
-import {
-    ProfileBioComponent
-} from "../components/elements/profile-bio/profile-bio.component";
-import {
-    ProfileNameComponent
-} from "../components/elements/profile-name/profile-name.component";
-import {
-    GeneralPostComponent
-} from "../components/elements/general-post/general-post.component";
+import {directives} from "../directives/directives";
+import {elements} from "../components/elements/elements";
+import {pages} from "../components/pages/pages";
+import {services} from "../services/services";
 
 @NgModule({
     declarations: [
-        // Directives
-        UnoptimizedImageDirective,
-        UndraggableDirective,
-        SignupConsoleHandlerDirective,
-        AutoSignupConsoleDirective,
-        PasswordMatchHandlerDirective,
-        AutoPasswordMatchDirective,
-        AutoPopupDirective,
-        EmailValidatorDirective,
-        FilledFieldsHandlerDirective,
-        AgreeTermsDirective,
-        // Elements
-        NavBarComponent,
-        SideNavBarComponent,
-        SideNavBarButtonComponent,
-        ConsoleComponent,
-        ConsoleInputComponent,
         AppComponent,
-        SsFooterComponent,
-        HeadComponent,
-        SsButtonComponent,
-        ConsolePopupComponent,
-        TypableTextComponent,
-        BlinkingCursorComponent,
-        SsImgComponent,
-        AuthBubbleComponent,
-        AuthTypeSelectorComponent,
-        HeadlineButtonComponent,
-        ProfileHeadlineButtonsComponent,
-        ProfileBioComponent,
-        ProfileNameComponent,
-        GeneralPostComponent,
-        // Pages
-        HomeComponent,
-        AuthorizeComponent,
-        ProfileComponent
+        ...elements,
+        ...directives,
+        ...pages,
     ],
-    imports: [BrowserModule,
-              BrowserAnimationsModule,
-              FormsModule,
-              AppRouting,
-              RouterOutlet,
-              NgOptimizedImage,
-              HttpClientModule],
-    providers: [SignupHandlerService,
-                PasswordMatchHandlerService,
-                EmailValidatorService,
-                ConsoleService,
-                ErrorHandlerService,
-                HashPasswordService,
-                SignupService,
-                AgreeTermsService,
-                provideHttpClient(withFetch()),
-                FilledFieldsService],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        AppRouting,
+        RouterOutlet,
+        NgOptimizedImage,
+        HttpClientModule],
+    providers: [
+        ...services,
+        provideHttpClient(withFetch()),
+    ],
     bootstrap: [AppComponent],
     exports: [AppComponent],
     schemas: []
