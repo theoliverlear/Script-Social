6da822fa8b1011d5dbddb05bc8614611c2c56190
theoliverlear <sigwartholiver2@gmail.com>
2024-10-11 13:34:42 -0500

    Add profile-headline-buttons.component.ts for profile headline buttons functionality

---
+++ src/main/resources/static/script/angular/components/elements/profile-headline-buttons/profile-headline-buttons.component.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+import {
+    HeadlineButtonType
+} from "../headline-button/models/HeadlineButtonType";
+
+@Component({
+    selector: 'profile-headline-buttons',
+    templateUrl: './profile-headline-buttons.component.html',
+    styleUrls: ['./profile-headline-buttons-style.component.css']
+})
+export class ProfileHeadlineButtonsComponent {
+    constructor() {
+        console.log('ProfileHeadlineButtonsComponent loaded');
+    }
+
+    protected readonly HeadlineButtonType = HeadlineButtonType;
+}
