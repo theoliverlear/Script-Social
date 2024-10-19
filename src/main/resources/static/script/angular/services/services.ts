0d45037dc7d1ce87eb860386439205ad9cc20016
theoliverlear <sigwartholiver2@gmail.com>
2024-10-18 19:48:05 -0500

    Add PostService to services.ts and update import path for SignupService in services.ts

--- src/main/resources/static/script/angular/services/services.ts
+++ src/main/resources/static/script/angular/services/services.ts
@@ -7,9 +7,10 @@ import {HashPasswordService} from "./hash-password.service";
 import {
     PasswordMatchHandlerDirective
 } from "../directives/password-match-handler.directive";
-import {SignupService} from "./signup.service";
+import {SignupService} from "./server/signup.service";
 import {SignupHandlerService} from "./signup-handler.service";
 import {SwipeService} from "./swipe.service";
+import {PostService} from "./server/post.service";
 
 export const services = [
     AgreeTermsService,
@@ -19,6 +20,7 @@ export const services = [
     FilledFieldsService,
     HashPasswordService,
     PasswordMatchHandlerDirective,
+    PostService,
     SignupService,
     SignupHandlerService,
     SwipeService
