748c77b0f924dd9188f2f48c1e8a795af79078fd
theoliverlear <sigwartholiver2@gmail.com>
2024-10-20 18:22:15 -0500

    Edit to utilize GeneralPost model and add error handling in post.service.ts

--- angular/services/server/post.service.ts
+++ angular/services/server/post.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {ErrorHandlerService} from "../error-handler.service";
-import {map, Observable} from "rxjs";
+import {catchError, map, Observable} from "rxjs";
 import {
-    GeneralPostComponent
-} from "../../components/elements/general-post/general-post.component";
+    GeneralPost
+} from "../../components/elements/general-post/models/GeneralPost";
 
 @Injectable({
     providedIn: 'root'
@@ -14,22 +14,27 @@ export class PostService {
                 private errorHandlerService: ErrorHandlerService) {
         console.log('PostService loaded');
     }
-    getPostsFromServer(userId: number): Observable<GeneralPostComponent[]> {
+    getPostsFromServer(userId: number): Observable<GeneralPost[]> {
         // TODO: Add some sort of default HttpHeaders to reduce repetition.
         const httpOptions: { headers: HttpHeaders } = {
             headers: new HttpHeaders({
                 'Content-Type': 'application/json'
             })
         };
-        return this.http.get<GeneralPostComponent[]>('/post/get/' + userId, httpOptions)
+        return this.http.get<GeneralPost[]>('/post/get/' + userId, httpOptions)
             .pipe(
-                map(response => response.map(postData =>
-                    this.createPostComponent(postData.postText, postData.userId, postData.postId, new Date(postData.postedDate))
-                )),
-                this.errorHandlerService.handleError<GeneralPostComponent[]>('getPostsFromServer', [])
+                map(response => this.getPostsFromResponse(response)),
+                catchError(this.errorHandlerService.handleError<GeneralPost[]>('getPostsFromServer', []))
             );
     }
-    createPostComponent(postText: string, userId: number, postId: number, postedDate: Date): GeneralPostComponent {
-        return new GeneralPostComponent(postText, userId, postId, postedDate);
+    getPostsFromResponse(response: any[]): GeneralPost[] {
+        return response.map(postData => (
+            new GeneralPost(
+                postData.postText,
+                postData.userId,
+                postData.postId,
+                new Date(postData.postedDate)
+            )
+        ));
     }
 }
