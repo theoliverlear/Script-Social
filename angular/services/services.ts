af3f8136b3e534b28fdfd1c5bb20bf2ad4d66469
theoliverlear <sigwartholiver2@gmail.com>
2024-10-20 18:24:59 -0500

    Refactor PasswordMatchHandler from directive to service and rearrange list to match directory structure in services.ts

--- angular/services/services.ts
+++ angular/services/services.ts
@@ -4,24 +4,24 @@ import {EmailValidatorService} from "./email-validator.service";
 import {ErrorHandlerService} from "./error-handler.service";
 import {FilledFieldsService} from "./filled-fields.service";
 import {HashPasswordService} from "./hash-password.service";
-import {
-    PasswordMatchHandlerDirective
-} from "../directives/password-match-handler.directive";
 import {SignupService} from "./server/signup.service";
 import {SignupHandlerService} from "./signup-handler.service";
 import {SwipeService} from "./swipe.service";
 import {PostService} from "./server/post.service";
+import {PasswordMatchHandlerService} from "./password-match-handler.service";
 
 export const services = [
+    // Server
+    PostService,
+    SignupService,
+    // Other
     AgreeTermsService,
     ConsoleService,
     EmailValidatorService,
     ErrorHandlerService,
     FilledFieldsService,
     HashPasswordService,
-    PasswordMatchHandlerDirective,
-    PostService,
-    SignupService,
+    PasswordMatchHandlerService,
     SignupHandlerService,
     SwipeService
 ]
