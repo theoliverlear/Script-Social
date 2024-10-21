0b74979466e33b1b3cba41d0eaadcf570a4be3bb
theoliverlear <sigwartholiver2@gmail.com>
2024-10-21 18:55:21 -0500

    Add ss-anchor.component.ts to create a reusable anchor component with configurable link properties

---
+++ angular/components/elements/ss-anchor/ss-anchor.component.ts
@@ -0,0 +1,33 @@
+import {
+    AfterContentInit,
+    Component,
+    ContentChild,
+    ElementRef,
+    Input
+} from "@angular/core";
+import {TagType} from "../../../models/TagType";
+import {TargetType} from "../../../models/TargetType";
+
+@Component({
+    selector: 'ss-anchor',
+    templateUrl: './ss-anchor.component.html',
+    styleUrls: ['./ss-anchor-style.component.css']
+})
+export class SsAnchorComponent implements AfterContentInit {
+    @Input() tagType: TagType = TagType.P;
+    @Input() hrefLink: string = '';
+    @Input() anchorText: string = '';
+    @Input() targetType: TargetType = TargetType.SELF;
+    @ContentChild('content') content: ElementRef;
+    hasContent: boolean = false;
+    constructor() {
+        console.log('SsAnchorComponent loaded');
+    }
+    navigateToLink(): void {
+        window.location.href = this.hrefLink;
+    }
+    ngAfterContentInit() {
+        this.hasContent = this.content && this.content.nativeElement.hasChildNodes();
+    }
+    protected readonly TagType = TagType;
+}
