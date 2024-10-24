f1d1d93e3a19ed54a75d84c97a489b1e994eb61c
theoliverlear <sigwartholiver2@gmail.com>
2024-10-23 19:37:25 -0500

    Edit openedWidth calculation to 11 percent width in sideNavBarSlideAnimationProperties in animationProperties.ts

--- angular/components/animations/animationProperties.ts
+++ angular/components/animations/animationProperties.ts
@@ -5,5 +5,6 @@ export type SideNavBarSlideAnimationProperties = {
 }
 export const sideNavBarSlideAnimationProperties: SideNavBarSlideAnimationProperties = {
     closedWidth: 'calc((4vw + 4vh) / 2 + 1.6em)',
-    openedWidth: 'calc((10vw + 10vh) / 2 + 4em)'
+    openedWidth: 'calc((11vw + 11vh) / 2 + 4.4em)'
+    // openedWidth: 'calc((10vw + 10vh) / 2 + 4em)'
 };
