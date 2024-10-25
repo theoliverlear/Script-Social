6f6e260386740695d26662bfcfb676237ecfb798
theoliverlear <sigwartholiver2@gmail.com>
2024-10-24 19:43:11 -0500

    Add import for GeneralPostButtonClickResponse and include TODO comment about creating button assets in general-post-buttons.component.ts

--- angular/components/elements/general-post-buttons/general-post-buttons.component.ts
+++ angular/components/elements/general-post-buttons/general-post-buttons.component.ts
@@ -2,6 +2,9 @@ import {Component} from "@angular/core";
 import {
     GeneralPostButtonType
 } from "../general-post-button/models/GeneralPostButtonType";
+import {
+    GeneralPostButtonClickResponse
+} from "../general-post-button/models/GeneralPostButtonClickResponse";
 
 @Component({
     selector: 'general-post-buttons',
@@ -9,9 +12,12 @@ import {
     styleUrls: ['./general-post-buttons-style.component.css']
 })
 export class GeneralPostButtonsComponent {
+    // TODO: Create constant assets for buttons which hold their properties
+    //       and simplifies embedded buttons' initialization.
     constructor() {
         console.log('GeneralPostButtonsComponent loaded');
     }
 
     protected readonly GeneralPostButtonType = GeneralPostButtonType;
+    protected readonly GeneralPostButtonClickResponse = GeneralPostButtonClickResponse;
 }
