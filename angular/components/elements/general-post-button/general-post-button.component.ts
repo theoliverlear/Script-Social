0d8c13a0da8f5c1f5d7bf7624f18c1f518d3e38b
theoliverlear <sigwartholiver2@gmail.com>
2024-10-24 19:39:28 -0500

    Add buttonType and clickResponse inputs to handle new post interaction logic in general-post-button.component.ts

--- angular/components/elements/general-post-button/general-post-button.component.ts
+++ angular/components/elements/general-post-button/general-post-button.component.ts
@@ -1,4 +1,5 @@
 import {
+    AfterViewInit,
     Component,
     ElementRef,
     Input,
@@ -8,51 +9,88 @@ import {
 import {
     ImageAsset,
     likeIcon,
-    whiteLikeIcon
+    whiteChatBubbleIcon,
+    whiteLikeIcon, whiteReplyIcon,
+    whiteRepostIcon,
+    whiteShareIcon
 } from "../../../assets/imageAssets";
 import {GeneralPostButtonType} from "./models/GeneralPostButtonType";
 import {SsImgComponent} from "../ss-img/ss-img.component";
+import {
+    GeneralPostButtonClickResponse
+} from "./models/GeneralPostButtonClickResponse";
 
 @Component({
     selector: 'general-post-button',
     templateUrl: './general-post-button.component.html',
     styleUrls: ['./general-post-button-style.component.css']
 })
-export class GeneralPostButtonComponent {
-    @Input() generalPostButtonType: GeneralPostButtonType;
+export class GeneralPostButtonComponent implements AfterViewInit {
+    @Input() buttonType: GeneralPostButtonType;
+    @Input() clickResponse: GeneralPostButtonClickResponse;
+    numInteractions: number = 0;
     isClicked: boolean = false;
     @ViewChild(('buttonImage')) buttonImage: SsImgComponent;
     constructor(private renderer: Renderer2, private element: ElementRef) {
         console.log('GeneralPostButtonComponent loaded');
     }
+    ngAfterViewInit() {
+        if (this.clickResponse === GeneralPostButtonClickResponse.QUICK_TRIGGER) {
+            this.buttonImage.addClassToImageElement('quick-trigger');
+        }
+    }
+
     toggleClicked() {
-        this.isClicked = !this.isClicked;
-        if (this.isClicked) {
-            this.renderer.addClass(this.element.nativeElement, 'clicked');
-        } else {
-            this.renderer.removeClass(this.element.nativeElement, 'clicked');
+        if (this.clickResponse === GeneralPostButtonClickResponse.TOGGLE) {
+            this.isClicked = !this.isClicked;
+            if (this.isClicked) {
+                this.renderer.addClass(this.element.nativeElement, 'clicked');
+            } else {
+                this.renderer.removeClass(this.element.nativeElement, 'clicked');
+            }
         }
         // this.buttonImage.imageAsset = this.toggleClickImageAsset();
     }
     getImageAsset(): ImageAsset {
         console.log('REACHED GET IMAGE ASSET');
-        switch (this.generalPostButtonType) {
+        switch (this.buttonType) {
             case GeneralPostButtonType.LIKE:
                 return whiteLikeIcon;
+            case GeneralPostButtonType.COMMENT:
+                return whiteReplyIcon;
+            case GeneralPostButtonType.REPOST:
+                return whiteRepostIcon;
+            case GeneralPostButtonType.SHARE:
+                return whiteShareIcon;
+            case GeneralPostButtonType.CHAT:
+                return whiteChatBubbleIcon;
             default:
                 return whiteLikeIcon;
         }
     }
+    isQuickTrigger(): boolean {
+        return this.clickResponse === GeneralPostButtonClickResponse.QUICK_TRIGGER;
+    }
+    isNumberedButton(): boolean {
+        switch (this.buttonType) {
+            case GeneralPostButtonType.LIKE:
+            case GeneralPostButtonType.COMMENT:
+            case GeneralPostButtonType.REPOST:
+                return true;
+            default:
+                return false;
+        }
+    }
     toggleClickImageAsset() {
         if (this.isClicked) {
-            switch (this.generalPostButtonType) {
+            switch (this.buttonType) {
                 case GeneralPostButtonType.LIKE:
                     return likeIcon;
                 default:
                     return likeIcon;
             }
         } else {
-            switch (this.generalPostButtonType) {
+            switch (this.buttonType) {
                 case GeneralPostButtonType.LIKE:
                     return whiteLikeIcon;
                 default:
