6725d8ed0bda6e332de81ccf07fdb2747da35c47
theoliverlear <sigwartholiver2@gmail.com>
2024-10-26 16:05:56 -0500

    Rename AutoPopupDirective to AutoAuthPopupDirective and add AutoEmptyPostHandlerDirective and EmptyPostHandlerDirective in directives.ts

--- angular/directives/directives.ts
+++ angular/directives/directives.ts
@@ -1,6 +1,6 @@
 import {AgreeTermsDirective} from "./agree-terms.directive";
 import {AutoPasswordMatchDirective} from "./auto-password-match.directive";
-import {AutoPopupDirective} from "./auto-popup.directive";
+import {AutoAuthPopupDirective} from "./auto-auth-popup.directive";
 import {AutoSignupConsoleDirective} from "./auto-signup-console.directive";
 import {EmailValidatorDirective} from "./email-validator.directive";
 import {
@@ -14,13 +14,19 @@ import {
 } from "./signup-console-handler.directive";
 import {UndraggableDirective} from "./undraggable.directive";
 import {UnoptimizedImageDirective} from "./unoptimized-image.directive";
+import {EmptyPostHandlerDirective} from "./empty-post-handler.directive";
+import {
+    AutoEmptyPostHandlerDirective
+} from "./auto-empty-post-handler.directive";
 
 export const directives = [
     AgreeTermsDirective,
+    AutoAuthPopupDirective,
+    AutoEmptyPostHandlerDirective,
     AutoPasswordMatchDirective,
-    AutoPopupDirective,
     AutoSignupConsoleDirective,
     EmailValidatorDirective,
+    EmptyPostHandlerDirective,
     FilledFieldsHandlerDirective,
     PasswordMatchHandlerDirective,
     SignupConsoleHandlerDirective,
