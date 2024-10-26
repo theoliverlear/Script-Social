bdec7d9fe5197e853c965ac129e6f1ea0b5bb0da
theoliverlear <sigwartholiver2@gmail.com>
2024-10-26 16:20:56 -0500

    Add event emitters and text input handling to GeneralPostCommentInputComponent in general-post-comment-input.component.ts

--- angular/components/elements/general-post-comment-input/general-post-comment-input.component.ts
+++ angular/components/elements/general-post-comment-input/general-post-comment-input.component.ts
@@ -1,12 +1,73 @@
-import {Component} from "@angular/core";
+import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {
+    GeneralPostButtonClickResponse
+} from "../general-post-button/models/GeneralPostButtonClickResponse";
+import {
+    GeneralPostButtonType
+} from "../general-post-button/models/GeneralPostButtonType";
+import {ElementSize} from "../../../models/ElementSize";
+import {PostService} from "../../../services/server/post.service";
+import {
+    ConsolePostPopup
+} from "../general-post-comment/models/ConsolePostPopup";
+import {EmptyPostService} from "../../../services/empty-post.service";
+import {fadeAnimation} from "../../animations/animations";
 
 @Component({
     selector: 'general-post-comment-input',
     templateUrl: './general-post-comment-input.component.html',
-    styleUrls: ['./general-post-comment-input-style.component.css']
+    styleUrls: ['./general-post-comment-input-style.component.css'],
+    animations: [
+        fadeAnimation
+    ]
 })
-export class GeneralPostCommentInputComponent {
-    constructor() {
+export class GeneralPostCommentInputComponent implements OnInit {
+    @Input() isOpened: boolean = false;
+    @Output() isOpenedChange: EventEmitter<boolean> = new EventEmitter();
+    @Output() postPopupChange: EventEmitter<ConsolePostPopup> = new EventEmitter();
+    postPopup: ConsolePostPopup | null = null;
+    inputText: string = '';
+    constructor(private postService: PostService,
+                private emptyPostService: EmptyPostService) {
         console.log('GeneralPostCommentInputComponent loaded');
     }
+    ngOnInit() {
+        this.emptyPostService.emptyPost$.subscribe((postPopup: ConsolePostPopup): void => {
+            this.postPopup = postPopup;
+        });
+    }
+
+    sendPostToServer() {
+        console.log('Sending post to server');
+        let isEmptyPost = this.emptyPostService.isPostTextEmpty(this.inputText);
+        console.log('isEmptyPost: ' + isEmptyPost);
+        if (isEmptyPost) {
+            this.postPopup = ConsolePostPopup.EMPTY;
+            this.postPopupChange.emit(ConsolePostPopup.EMPTY);
+        } else {
+            // TODO: Create a setAndEmit() method.
+            this.postPopup = null;
+            this.postPopupChange.emit(null);
+            // Sends the post to the server
+        }
+    }
+    handleTextChange(text: string) {
+        console.log('Text changed: ' + text);
+        if (this.inputText.length < text.length) {
+            this.postPopup = null;
+            this.postPopupChange.emit(null);
+        }
+        this.inputText = text;
+    }
+    openCommentInput() {
+        this.isOpened = true;
+    }
+    closeCommentInput() {
+        this.isOpened = false;
+        this.postPopup = null;
+        this.postPopupChange.emit(null);
+    }
+    protected readonly GeneralPostButtonClickResponse = GeneralPostButtonClickResponse;
+    protected readonly GeneralPostButtonType = GeneralPostButtonType;
+    protected readonly ElementSize = ElementSize;
 }
