1d3cb199a0b2edc0a26123334ec6c6b9744b791e
theoliverlear <sigwartholiver2@gmail.com>
2024-10-30 16:52:19 -0500

    Add MessagesComponent to the pages array in pages.ts for inclusion in the component registry

--- angular/components/pages/pages.ts
+++ angular/components/pages/pages.ts
@@ -1,9 +1,11 @@
 import {AuthorizeComponent} from "./authorize/authorize.component";
 import {HomeComponent} from "./home/home.component";
 import {ProfileComponent} from "./profile/profile.component";
+import {MessagesComponent} from "./messages/messages.component";
 
 export const pages = [
     AuthorizeComponent,
     HomeComponent,
+    MessagesComponent,
     ProfileComponent
 ]
