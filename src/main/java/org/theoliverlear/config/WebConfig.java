bf0706f4ee1b7db2d44f47aade9ded22ec8489b0
theoliverlear <sigwartholiver2@gmail.com>
2024-10-30 16:53:20 -0500

    Add CORS configuration in WebConfig.java to allow requests from localhost:4200

--- src/main/java/org/theoliverlear/config/WebConfig.java
+++ src/main/java/org/theoliverlear/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.theoliverlear.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/templates/**")
                 .addResourceLocations("classpath:/static/templates/");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
