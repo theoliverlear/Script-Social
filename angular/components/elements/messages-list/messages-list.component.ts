96401444fc59e518583d93ebcd6324a15a76723d
theoliverlear <sigwartholiver2@gmail.com>
2024-11-01 16:56:09 -0500

    Add messages-list.component.ts to display a list of user's messages in the Angular application

---
+++ angular/components/elements/messages-list/messages-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from "@angular/core";
+import {Message} from "../../../models/Message";
+
+@Component({
+    selector: 'messages-list',
+    templateUrl: './messages-list.component.html',
+    styleUrls: ['./messages-list-style.component.css']
+})
+export class MessagesListComponent {
+    testMessage: Message = new Message(0,
+                                       'John Doe',
+                                       new Date(),
+                                       `Yo Mista White. I see you got the
+                                        semicolons bitch. Finally, you stopped
+                                        the errors, like, computationally.
+                                        That's like, science, Mista White.`);
+    constructor() {
+        console.log('MessagesListComponent loaded');
+    }
+}
