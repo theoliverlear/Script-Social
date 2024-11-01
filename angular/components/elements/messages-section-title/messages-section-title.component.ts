9bb8c391433a748c3627906eef85d7ea4e7284f2
theoliverlear <sigwartholiver2@gmail.com>
2024-11-01 17:00:29 -0500

    Add messages-section-title.component.ts to handle the display of the messages section title in the Angular app

---
+++ angular/components/elements/messages-section-title/messages-section-title.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from "@angular/core";
+import {MessagesSectionTitle} from "./models/MessagesSectionTitle";
+
+@Component({
+    selector: 'messages-section-title',
+    templateUrl: './messages-section-title.component.html',
+    styleUrls: ['./messages-section-title-style.component.css']
+})
+export class MessagesSectionTitleComponent {
+    @Input() titleText: MessagesSectionTitle;
+    constructor() {
+        console.log('MessagesSectionTitleComponent loaded');
+    }
+}
