40237a4fec52e257e1381d51300a967856077cc8
theoliverlear <sigwartholiver2@gmail.com>
2024-11-01 16:15:29 -0500

    Add Message.ts to define the properties of user messages in the Angular application

---
+++ angular/models/Message.ts
@@ -0,0 +1,15 @@
+export class Message {
+    id: number;
+    usersName: string;
+    dateSent: Date;
+    messageText: string;
+    constructor(id: number = 0,
+        usersName: string = '',
+        dateSent: Date = new Date(),
+        messageText: string = '') {
+        this.id = id;
+        this.usersName = usersName;
+        this.dateSent = dateSent;
+        this.messageText = messageText;
+    }
+}
