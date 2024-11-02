29ad53e16fb9fd25c2ae3707619bf96285a98d48
theoliverlear <sigwartholiver2@gmail.com>
2024-11-02 17:13:34 -0500

    Add after view initialization, input properties, click responses, and icon handling in connections-list-button.component.ts

--- angular/components/elements/connections-list-button/connections-list-button.component.ts
+++ angular/components/elements/connections-list-button/connections-list-button.component.ts
@@ -1,12 +1,40 @@
-import {Component} from "@angular/core";
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    Input,
+    ViewChild
+} from "@angular/core";
+import {addIcon, confirmIcon} from "../../../assets/imageAssets";
+import {ConnectionsListButtonType} from "./models/ConnectionsListButtonType";
+import {SsImgComponent} from "../ss-img/ss-img.component";
 
 @Component({
     selector: 'connections-list-button',
     templateUrl: './connections-list-button.component.html',
     styleUrls: ['./connections-list-button-style.component.css']
 })
-export class ConnectionsListButtonComponent {
-    constructor() {
+export class ConnectionsListButtonComponent implements AfterViewInit {
+    @Input() buttonType: ConnectionsListButtonType;
+    @ViewChild('buttonImage') buttonImage: SsImgComponent;
+    isClicked: boolean = false;
+    constructor(private changeDetector: ChangeDetectorRef) {
         console.log('ConnectionsListButtonComponent loaded');
+    }
+    toggleClicked() {
+        this.isClicked = !this.isClicked
+    }
+    ngAfterViewInit() {
+        switch (this.buttonType) {
+            case ConnectionsListButtonType.ADD:
+                this.buttonImage.imageAsset = addIcon;
+                break;
+            case ConnectionsListButtonType.CONFIRM:
+                this.buttonImage.imageAsset = confirmIcon;
+                break;
+            default:
+                console.error('Invalid button type');
+        }
+        this.changeDetector.detectChanges();
     }
 }
