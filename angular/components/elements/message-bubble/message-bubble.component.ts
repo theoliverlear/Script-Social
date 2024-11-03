27b0d025ce241b14f9e8027bb5a51b339cc1b6b3
theoliverlear <sigwartholiver2@gmail.com>
2024-11-03 16:13:51 -0600

    Add elementSize and imageAsset inputs to MessageBubbleComponent in message-bubble.component.ts for enhanced customization

--- angular/components/elements/message-bubble/message-bubble.component.ts
+++ angular/components/elements/message-bubble/message-bubble.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from "@angular/core";
 import {Message} from "../../../models/Message";
+import {ElementSize} from "../../../models/ElementSize";
+import {defaultAvatar, ImageAsset} from "../../../assets/imageAssets";
 
 @Component({
     selector: 'message-bubble',
@@ -8,7 +10,19 @@ import {Message} from "../../../models/Message";
 })
 export class MessageBubbleComponent {
     @Input() message: Message;
+    @Input() elementSize: ElementSize = ElementSize.MEDIUM;
+    @Input() imageAsset: ImageAsset = defaultAvatar;
     constructor() {
         console.log('MessageBubbleComponent loaded');
     }
+    isMediumSize(): boolean {
+        return this.elementSize === ElementSize.MEDIUM;
+    }
+    isLargeSize(): boolean {
+        return this.elementSize === ElementSize.LARGE;
+    }
+    getSizeClass(): string {
+        return this.elementSize;
+    }
+    protected readonly ElementSize = ElementSize;
 }
