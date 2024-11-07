c491c8542a1c10dd762a9f2c54fadef8cc3fcc0f
theoliverlear <sigwartholiver2@gmail.com>
2024-11-07 17:56:58 -0600

    Add tail position functionality and conditional rendering in message-bubble.component.ts

--- angular/components/elements/message-bubble/message-bubble.component.ts
+++ angular/components/elements/message-bubble/message-bubble.component.ts
@@ -1,18 +1,21 @@
-import {Component, Input} from "@angular/core";
+import {Component, ElementRef, Input, OnInit, Renderer2} from "@angular/core";
 import {Message} from "../../../models/Message";
 import {ElementSize} from "../../../models/ElementSize";
 import {defaultAvatar, ImageAsset} from "../../../assets/imageAssets";
+import {BubbleTailSide} from "./models/BubbleTailSide";
 
 @Component({
     selector: 'message-bubble',
     templateUrl: './message-bubble.component.html',
     styleUrls: ['./message-bubble-style.component.css']
 })
-export class MessageBubbleComponent {
+export class MessageBubbleComponent implements OnInit {
     @Input() message: Message;
     @Input() elementSize: ElementSize = ElementSize.MEDIUM;
     @Input() imageAsset: ImageAsset = defaultAvatar;
-    constructor() {
+    @Input() tailSide: BubbleTailSide;
+    constructor(private element: ElementRef,
+                private renderer: Renderer2) {
         console.log('MessageBubbleComponent loaded');
     }
     isMediumSize(): boolean {
@@ -23,6 +26,14 @@ export class MessageBubbleComponent {
     }
     getSizeClass(): string {
         return this.elementSize;
+    }
+    ngOnInit() {
+        this.applyTailClass();
+    }
+    applyTailClass(): void {
+        if (this.tailSide) {
+            this.renderer.addClass(this.element.nativeElement, this.tailSide);
+        }
     }
     protected readonly ElementSize = ElementSize;
 }
