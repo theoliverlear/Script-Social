25e913769c23e56dbd96fa33bf32ecdf6746c4fa
theoliverlear <sigwartholiver2@gmail.com>
2024-11-07 17:51:49 -0600

    Update HomeComponent in home.component.ts to include new animations and child components viewers

--- angular/components/pages/home/home.component.ts
+++ angular/components/pages/home/home.component.ts
@@ -1,15 +1,15 @@
-import {Component, OnInit} from "@angular/core";
+import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
 import {ConsoleType} from "../../elements/console/models/ConsoleType";
 import {TagType} from "../../../models/html/TagType";
 import {
     colinFredricksonPicture,
     markLilianoPicture,
-    scriptSocialLogoCropped
+    scriptSocialLogoCropped, tatianaRamosPicture
 } from "../../../assets/imageAssets";
 import {TypeSpeed} from "../../../models/TypeSpeed";
 import {ElementSize} from "../../../models/ElementSize";
 import {
-    scrollFadeInAnimation
+    fadeIn
 } from "../../animations/animations";
 import {
     homeMessageInviteSeniorDevText,
@@ -17,25 +17,39 @@ import {
     originalCodeText
 } from "../../../assets/textAssets";
 import {Message} from "../../../models/Message";
+import {
+    BubbleTailSide
+} from "../../elements/message-bubble/models/BubbleTailSide";
+import {
+    homeInviteSeniorDevMessageFadeInAnimation,
+    homeRefactorMessageFadeInAnimation,
+    promoCodeFadeInAnimation
+} from "../../animations/animationProperties";
+import {
+    TypingTextComponent
+} from "../../elements/typing-text/typing-text.component";
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home-style.component.css'],
     animations: [
-        scrollFadeInAnimation
+        fadeIn
     ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
     homeRefactorMessage: Message = new Message(0, 'Mark Liliano', new Date(), homeMessageRefactorText);
     homeInviteSeniorDevMessage: Message = new Message(0, 'Colin Fredrickson', new Date(), homeMessageInviteSeniorDevText);
+    @ViewChild('communicateYourCodeTypingText') communicateYourCodeTypingText: TypingTextComponent;
     constructor() {
         console.log('HomeComponent loaded');
     }
-    ngOnInit() {
-
+    ngAfterViewInit() {
+        this.typeCommunicateYourCodeText();
     }
-
+    typeCommunicateYourCodeText(): void {
+        this.communicateYourCodeTypingText.typeTextWithDelay(1500);
+    }
     protected readonly ConsoleType = ConsoleType;
     protected readonly TagType = TagType;
     protected readonly scriptSocialLogoCropped = scriptSocialLogoCropped;
@@ -44,4 +58,9 @@ export class HomeComponent implements OnInit {
     protected readonly originalCodeText = originalCodeText;
     protected readonly colinFredricksonPicture = colinFredricksonPicture;
     protected readonly markLilianoPicture = markLilianoPicture;
+    protected readonly BubbleTailSide = BubbleTailSide;
+    protected readonly promoCodeFadeInAnimation = promoCodeFadeInAnimation;
+    protected readonly homeRefactorMessageFadeInAnimation = homeRefactorMessageFadeInAnimation;
+    protected readonly homeInviteSeniorDevMessageFadeInAnimation = homeInviteSeniorDevMessageFadeInAnimation;
+    protected readonly tatianaRamosPicture = tatianaRamosPicture;
 }
