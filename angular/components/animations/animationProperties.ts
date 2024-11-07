62b1e0cbb982971a962a4fea5998b380cf63d1f0
theoliverlear <sigwartholiver2@gmail.com>
2024-11-07 17:33:54 -0600

    Add animation properties for homepage fade-in effects and abstract animations property types to models directory interfaces in animationProperties.ts

--- angular/components/animations/animationProperties.ts
+++ angular/components/animations/animationProperties.ts
@@ -1,10 +1,23 @@
-// TODO: Try to be less verbose while getting point across.
-export type SideNavBarSlideAnimationProperties = {
-    closedWidth: string;
-    openedWidth: string;
-}
+import {
+    SideNavBarSlideAnimationProperties
+} from "./models/SideNavBarSlideAnimationProperties";
+import {FadeInAnimationProperties} from "./models/FadeInAnimationProperties";
+
 export const sideNavBarSlideAnimationProperties: SideNavBarSlideAnimationProperties = {
     closedWidth: 'calc((4vw + 4vh) / 2 + 1.6em)',
     openedWidth: 'calc((11vw + 11vh) / 2 + 4.4em)'
     // openedWidth: 'calc((10vw + 10vh) / 2 + 4em)'
+};
+
+export const promoCodeFadeInAnimation: FadeInAnimationProperties = {
+    duration: '1200ms',
+    delay: '150ms'
+};
+export const homeRefactorMessageFadeInAnimation: FadeInAnimationProperties = {
+    duration: '1000ms',
+    delay: '4000ms'
+};
+export const homeInviteSeniorDevMessageFadeInAnimation: FadeInAnimationProperties = {
+    duration: '1000ms',
+    delay: '7500ms'
 };
