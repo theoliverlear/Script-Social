c0dd9e514e8b7e10e3b698823d84c2854ff65e01
theoliverlear <sigwartholiver2@gmail.com>
2024-11-07 18:11:35 -0600

    Add user-choice-bubble.component.ts to render user choice bubbles in the Angular application to represent an accepting or rejecting function associated with a user

---
+++ angular/components/elements/user-choice-bubble/user-choice-bubble.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input} from "@angular/core";
+import {
+    GeneralPostButtonClickResponse
+} from "../general-post-button/models/GeneralPostButtonClickResponse";
+import {
+    GeneralPostButtonType
+} from "../general-post-button/models/GeneralPostButtonType";
+import {ImageAsset} from "../../../assets/imageAssets";
+
+@Component({
+    selector: 'user-choice-bubble',
+    templateUrl: './user-choice-bubble.component.html',
+    styleUrls: ['./user-choice-bubble-style.component.css']
+})
+export class UserChoiceBubbleComponent {
+    @Input() imageAsset: ImageAsset;
+    @Input() usersName: string;
+    constructor() {
+        console.log('UserChoiceBubbleComponent loaded');
+    }
+
+    protected readonly GeneralPostButtonClickResponse = GeneralPostButtonClickResponse;
+    protected readonly GeneralPostButtonType = GeneralPostButtonType;
+}
