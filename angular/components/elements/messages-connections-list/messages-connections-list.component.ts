4a4c84cd41216df2f2cebb14234e95a56a1f37dd
theoliverlear <sigwartholiver2@gmail.com>
2024-11-08 15:01:16 -0600

    Add event handling to select and deselect connection bubbles in messages-connections-list.component.ts

--- angular/components/elements/messages-connections-list/messages-connections-list.component.ts
+++ angular/components/elements/messages-connections-list/messages-connections-list.component.ts
@@ -1,14 +1,37 @@
-import {Component} from "@angular/core";
-import {User} from "../../../models/user/User";
+import {
+    AfterViewInit,
+    Component,
+    QueryList,
+    ViewChildren
+} from "@angular/core";
+import {
+    ConnectionsListBubbleComponent
+} from "../connections-list-bubble/connections-list-bubble.component";
 
 @Component({
     selector: 'messages-connections-list',
     templateUrl: './messages-connections-list.component.html',
     styleUrls: ['./messages-connections-list-style.component.css']
 })
-export class MessagesConnectionsListComponent {
-    // connectionsList: User[];
+export class MessagesConnectionsListComponent implements AfterViewInit {
+    @ViewChildren(ConnectionsListBubbleComponent) connectionsListBubbles: QueryList<ConnectionsListBubbleComponent>;
     constructor() {
         console.log('MessagesConnectionsListComponent loaded');
+    }
+    ngAfterViewInit() {
+        this.subscribeToBubbleClickEvents();
+    }
+    subscribeToBubbleClickEvents() {
+        this.connectionsListBubbles.forEach((bubble) => {
+            bubble.clickEvent.subscribe(() => {
+                this.deselectAllConnections();
+                bubble.select();
+            });
+        });
+    }
+    deselectAllConnections(): void {
+        this.connectionsListBubbles.forEach((bubble) => {
+            bubble.deselect();
+        });
     }
 }
