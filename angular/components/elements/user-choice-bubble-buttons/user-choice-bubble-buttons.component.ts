8c438505849e64ea5b300c0b934e415db17a2385
theoliverlear <sigwartholiver2@gmail.com>
2024-11-07 18:13:33 -0600

    Add user-choice-bubble-buttons.component.ts for creating user action buttons in the Angular application

---
+++ angular/components/elements/user-choice-bubble-buttons/user-choice-bubble-buttons.component.ts
@@ -0,0 +1,21 @@
+import {Component} from "@angular/core";
+import {
+    GeneralPostButtonType
+} from "../general-post-button/models/GeneralPostButtonType";
+import {
+    GeneralPostButtonClickResponse
+} from "../general-post-button/models/GeneralPostButtonClickResponse";
+
+@Component({
+    selector: 'user-choice-bubble-buttons',
+    templateUrl: './user-choice-bubble-buttons.component.html',
+    styleUrls: ['./user-choice-bubble-buttons-style.component.css']
+})
+export class UserChoiceBubbleButtonsComponent {
+    constructor() {
+        console.log('UserChoiceBubbleButtonsComponent loaded');
+    }
+
+    protected readonly GeneralPostButtonType = GeneralPostButtonType;
+    protected readonly GeneralPostButtonClickResponse = GeneralPostButtonClickResponse;
+}
