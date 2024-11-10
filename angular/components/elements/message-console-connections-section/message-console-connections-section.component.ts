6c8f1056128f08b6d2232499df0684049e0254a0
theoliverlear <sigwartholiver2@gmail.com>
2024-11-10 17:07:40 -0600

    Add ViewChild and toggleConfirmButtonView method in message-console-connections-section.component.ts for button visibility control

--- angular/components/elements/message-console-connections-section/message-console-connections-section.component.ts
+++ angular/components/elements/message-console-connections-section/message-console-connections-section.component.ts
@@ -1,10 +1,13 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 import {
     MessagesSectionTitle
 } from "../messages-section-title/models/MessagesSectionTitle";
 import {
     ConnectionsListButtonType
 } from "../connections-list-button/models/ConnectionsListButtonType";
+import {
+    ConnectionsListButtonComponent
+} from "../connections-list-button/connections-list-button.component";
 
 @Component({
     selector: 'message-console-connections-section',
@@ -12,10 +15,14 @@ import {
     styleUrls: ['./message-console-connections-section-style.component.css']
 })
 export class MessageConsoleConnectionsSectionComponent {
+    @ViewChild('confirmButton') confirmButton: ConnectionsListButtonComponent;
     constructor() {
         console.log('MessageConsoleConnectionsSectionComponent loaded');
     }
-
+    toggleConfirmButtonView() {
+        console.log('Toggling confirm button view');
+        this.confirmButton.toggleVisibility();
+    }
     protected readonly MessagesSectionTitles = MessagesSectionTitle;
     protected readonly ConnectionsListButtonType = ConnectionsListButtonType;
 }
