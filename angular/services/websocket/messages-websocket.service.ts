0cbfc14580fe27c5e26b4dd580bc3ce191264671
theoliverlear <sigwartholiver2@gmail.com>
2024-11-13 16:25:26 -0600

    Add MessagesWebSocketService to handle incoming instant messages in messages-websocket.service.ts

---
+++ angular/services/websocket/messages-websocket.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from "@angular/core";
+import {Message} from "../../models/message/Message";
+import {WebSocketService} from "./websocket.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MessagesWebSocketService extends WebSocketService<Message> {
+    static readonly URL: string = '/messages/receiver';
+    constructor() {
+        super(MessagesWebSocketService.URL);
+        console.log('MessagesWebsocketService created');
+    }
+    onReceive(frame: any): void {
+        let messageJson: any = JSON.parse(frame.body);
+        const messageId: number = messageJson.id;
+        const usersName: string = messageJson.usersName;
+        const dateSent: Date = new Date(messageJson.dateSent);
+        const messageText: string = messageJson.messageText;
+        const message: Message = new Message(messageId, usersName, dateSent, messageText);
+        this.contentSubject.next(message);
+    }
+}
