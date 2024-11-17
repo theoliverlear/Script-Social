26256fc9578bcd499e6f37c08031430292fe0671
theoliverlear <sigwartholiver2@gmail.com>
2024-11-17 14:26:17 -0600

    Change generic type from Message to Communicative in messages-websocket.service.ts to better generalize message serving

--- angular/services/websocket/messages-websocket.service.ts
+++ angular/services/websocket/messages-websocket.service.ts
@@ -1,11 +1,12 @@
 import {Injectable} from "@angular/core";
 import {Message} from "../../models/message/Message";
 import {WebSocketService} from "./websocket.service";
+import {Communicative} from "../../models/message/Communicative";
 
 @Injectable({
     providedIn: 'root'
 })
-export class MessagesWebSocketService extends WebSocketService<Message> {
+export class MessagesWebSocketService extends WebSocketService<Communicative> {
     static readonly URL: string = '/messages/receiver';
     constructor() {
         super(MessagesWebSocketService.URL);
