6ab1e6ea3123bb65a5331406c9418c0cc4adb580
theoliverlear <sigwartholiver2@gmail.com>
2024-11-23 12:30:40 -0600

    Add login.service.ts to handle user login authentication with the server

---
+++ angular/services/server/login.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {ErrorHandlerService} from "../error-handler.service";
+import {HashPasswordService} from "../hash-password.service";
+import {httpOptions} from "./httpProperties";
+import {map} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginService {
+    constructor(private http: HttpClient,
+                private errorHandlerService: ErrorHandlerService,
+                private hashPasswordService: HashPasswordService) {
+        console.log('LoginService loaded');
+    }
+    sendLoginToServer(username: string, unhashedPassword: string) {
+        const hashedPassword: string = this.hashPasswordService.hashPassword(unhashedPassword);
+        const loginInfo = {
+            username: username,
+            password: hashedPassword
+        };
+        return this.http.post<{isAuthorized: boolean}>('http://localhost:8080/api/authorize/login', loginInfo, httpOptions)
+            .pipe(
+                map(response => {
+                    return response.isAuthorized;
+                }),
+                this.errorHandlerService.handleError('loginToServer')
+            )
+    }
+}
