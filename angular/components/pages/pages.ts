7f1ef2ac205ae7331e10e1308242d60a6d983975
theoliverlear <sigwartholiver2@gmail.com>
2024-11-22 19:34:44 -0600

    Add WelcomeComponent to pages array in pages.ts to inject welcome page functionality into the Angular application

--- angular/components/pages/pages.ts
+++ angular/components/pages/pages.ts
@@ -2,10 +2,12 @@ import {AuthorizeComponent} from "./authorize/authorize.component";
 import {HomeComponent} from "./home/home.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {MessagesComponent} from "./messages/messages.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
 export const pages = [
     AuthorizeComponent,
     HomeComponent,
     MessagesComponent,
-    ProfileComponent
+    ProfileComponent,
+    WelcomeComponent
 ]
