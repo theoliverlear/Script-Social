3a17c2756fc8f716fdc4394f4aac686358ca787d
theoliverlear <sigwartholiver2@gmail.com>
2024-11-23 12:34:57 -0600

    Add method to retrieve current user ID from server in user.service.ts

--- angular/services/server/user.service.ts
+++ angular/services/server/user.service.ts
@@ -1,10 +1,25 @@
 import {Injectable} from "@angular/core";
+import {ErrorHandlerService} from "../error-handler.service";
+import {HttpClient} from "@angular/common/http";
+import {catchError, map, Observable} from "rxjs";
+import {httpOptions} from "./httpProperties";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
-    constructor() {
+    private getCurrentUserIdUrl: string = 'http://localhost:8080/api/user/get/current/id';
+    constructor(private http: HttpClient,
+                private errorHandlerService: ErrorHandlerService) {
         console.log('UserService loaded');
+    }
+    getCurrentUserIdFromServer(): Observable<number> {
+        return this.http.get<{userId: number}>(this.getCurrentUserIdUrl, httpOptions)
+            .pipe(
+                map(response => {
+                    return response.userId;
+                }),
+                catchError(this.errorHandlerService.handleError<number>('getCurrentUserIdFromServer'))
+            );
     }
 }
