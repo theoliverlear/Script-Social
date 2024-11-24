eb610fa4e150778eacc892b6d2bc6fc2070137a8
theoliverlear <sigwartholiver2@gmail.com>
2024-11-23 21:31:32 -0600

    Add slide-in-element.service.ts to inject modular element sliding functionality into the Angular application

---
+++ angular/services/slide-in-element.service.ts
@@ -0,0 +1,32 @@
+import {
+    ElementRef,
+    Injectable
+} from "@angular/core";
+import {
+    animate,
+    AnimationBuilder, AnimationFactory,
+    AnimationPlayer, style
+} from "@angular/animations";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SlideInElementService {
+    constructor(private animationBuilder: AnimationBuilder) {
+        console.log('SlideInElementService loaded');
+    }
+    async slideInElement(element: ElementRef,
+                         slideDuration: string = '3s ease-out'): Promise<void> {
+        return new Promise((resolve) => {
+            const animation: AnimationFactory = this.animationBuilder.build([
+                style({ transform: 'translateY(100vh)' }),
+                animate(slideDuration, style({ transform: 'translateY(0%)' })),
+            ]);
+            const player: AnimationPlayer = animation.create(element.nativeElement);
+            player.onDone(() => {
+                resolve();
+            });
+            player.play();
+        });
+    }
+}
