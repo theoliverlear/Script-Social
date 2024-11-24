465a365b9dee3611b7a94013c7f83895b030f4df
theoliverlear <sigwartholiver2@gmail.com>
2024-11-23 21:36:51 -0600

    Add confetti effect and slide-in animation to the welcome banner on load in welcome.component.ts

--- angular/components/pages/welcome/welcome.component.ts
+++ angular/components/pages/welcome/welcome.component.ts
@@ -1,12 +1,31 @@
-import {Component} from "@angular/core";
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    ViewChild
+} from "@angular/core";
+import {ConfettiService} from "../../../services/confetti.service";
+import {
+    SlideInElementService
+} from "../../../services/slide-in-element.service";
 
 @Component({
-    selector: 'app-welcome',
+    selector: 'welcome',
     templateUrl: './welcome.component.html',
     styleUrls: ['./welcome-style.component.css']
 })
-export class WelcomeComponent {
-    constructor() {
+export class WelcomeComponent implements AfterViewInit {
+    @ViewChild('welcomeBanner', {read: ElementRef}) welcomeBanner: ElementRef;
+    constructor(private confettiService: ConfettiService,
+                private slideInElementService: SlideInElementService) {
         console.log('WelcomeComponent loaded');
+    }
+    ngAfterViewInit(): void {
+        console.log('Welcome banner element:', this.welcomeBanner);
+        this.confettiService.burstEffect();
+        this.slideInWelcomeBanner();
+    }
+    async slideInWelcomeBanner(): Promise<void> {
+        await this.slideInElementService.slideInElement(this.welcomeBanner);
     }
 }
