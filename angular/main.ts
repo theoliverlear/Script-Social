859c3dfccdec2d9a285fe1a4d53eda74f08b67a3
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:16:05 -0600

    Enable production mode in main.ts by uncommenting environment check

--- angular/main.ts
+++ angular/main.ts
@@ -1,12 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ScriptSocialAppModule } from "./modules/script-social-app.module";
-import '@angular/compiler'; // Ensure this is available for JIT
+import '@angular/compiler';
 import { environment } from "./environments/environment";
 import { enableProdMode } from "@angular/core";
 
-// if (environment.production) {
-//     enableProdMode();
-// }
+if (environment.production) {
+    enableProdMode();
+}
 
 console.log('main.ts loaded');
 
