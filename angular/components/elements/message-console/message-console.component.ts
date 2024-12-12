f46a68f167c1b241599fc5191d3b3ad804d57fe6
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:17:21 -0600

    Add WebSocket service integration and begin sendMessageToServer method in message-console.component.ts

--- angular/components/elements/message-console/message-console.component.ts
+++ angular/components/elements/message-console/message-console.component.ts
@@ -1,4 +1,10 @@
-import {Component} from "@angular/core";
+import {
+    Component,
+} from "@angular/core";
+import {
+    MessagesWebSocketService
+} from "../../../services/websocket/messages-websocket.service";
+import {ClientMessage} from "../../../models/message/ClientMessage";
 
 @Component({
     selector: 'message-console',
@@ -6,8 +12,13 @@ import {Component} from "@angular/core";
     styleUrls: ['./message-console-style.component.css']
 })
 export class MessageConsoleComponent {
-    constructor() {
+    constructor(private messagesWebsocketService: MessagesWebSocketService) {
         console.log('MessageConsoleComponent loaded');
     }
 
+    sendMessageToServer() {
+        const testMessageText = 'Hey'; // Somehow get the message text from the input field
+        const testUserId = 1; // Somehow get the user id from the session
+        this.messagesWebsocketService.send(new ClientMessage(testUserId, testMessageText));
+    }
 }
