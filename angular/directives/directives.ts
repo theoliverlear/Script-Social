67ddeaf8f16d767479ac7c70855503d141b9b364
theoliverlear <sigwartholiver2@gmail.com>
2025-05-27 20:23:12 -0500

    Reorganize directive imports in directives.ts to reflect 'auto' and 'validation' directory structure

--- angular/directives/directives.ts
+++ angular/directives/directives.ts
@@ -1,23 +1,23 @@
-import {AgreeTermsDirective} from "./agree-terms.directive";
-import {AutoPasswordMatchDirective} from "./auto-password-match.directive";
-import {AutoAuthPopupDirective} from "./auto-auth-popup.directive";
-import {AutoSignupConsoleDirective} from "./auto-signup-console.directive";
-import {EmailValidatorDirective} from "./email-validator.directive";
+import {AgreeTermsDirective} from "./auto/agree-terms.directive";
+import {AutoPasswordMatchDirective} from "./auto/auto-password-match.directive";
+import {AutoAuthPopupDirective} from "./auto/auto-auth-popup.directive";
+import {AutoSignupConsoleDirective} from "./auto/auto-signup-console.directive";
+import {EmailValidatorDirective} from "./validation/email-validator.directive";
 import {
     FilledFieldsHandlerDirective
-} from "./filled-fields-handler.directive";
+} from "./validation/filled-fields-handler.directive";
 import {
     PasswordMatchHandlerDirective
-} from "./password-match-handler.directive";
+} from "./validation/password-match-handler.directive";
 import {
     SignupConsoleHandlerDirective
 } from "./signup-console-handler.directive";
 import {UndraggableDirective} from "./undraggable.directive";
 import {UnoptimizedImageDirective} from "./unoptimized-image.directive";
-import {EmptyPostHandlerDirective} from "./empty-post-handler.directive";
+import {EmptyPostHandlerDirective} from "./validation/empty-post-handler.directive";
 import {
     AutoEmptyPostHandlerDirective
-} from "./auto-empty-post-handler.directive";
+} from "./auto/auto-empty-post-handler.directive";
 import {SyntaxHighlightDirective} from "./syntax-highlight.directive";
 
 export const directives = [
