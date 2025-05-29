d2e9bcb71fad3adb9a23dec972090a569531028b
theoliverlear <sigwartholiver2@gmail.com>
2025-05-29 17:51:47 -0500

    Edit httpProperties.ts to introduce `HttpOptions` type and credentials support

--- angular/services/server/http/httpProperties.ts
+++ angular/services/server/http/httpProperties.ts
@@ -1,7 +1,18 @@
 import {HttpHeaders} from "@angular/common/http";
 
-export const httpOptions: { headers: HttpHeaders } = {
+export type HttpOptions = {
+    headers: HttpHeaders,
+    withCredentials?: boolean
+}
+
+export const httpOptions: HttpOptions = {
     headers: new HttpHeaders({
         'Content-Type': 'application/json'
     })
+};
+export const httpOptionsWithCredentials: HttpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    }),
+    withCredentials: true
 };
