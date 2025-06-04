058d2305a3fcfa241a9b83944fbe01b93c8f087b
theoliverlear <sigwartholiver2@gmail.com>
2025-06-04 18:52:24 -0500

    Add welcome-banner.component.ts to be an Angular component for the welcome-banner element in the Angular application

---
+++ angular/components/elements/element-group-welcome/welcome-banner/welcome-banner.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'welcome-banner',
+    templateUrl: './welcome-banner.component.html',
+    styleUrls: ['./welcome-banner.component.css']
+})
+export class WelcomeBannerComponent {
+    constructor() {
+
+    }
+}
