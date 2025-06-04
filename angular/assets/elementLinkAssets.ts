78dec71d81a598ffac167006407c74316dc38209
theoliverlear <sigwartholiver2@gmail.com>
2025-06-04 18:12:01 -0500

    Refactor elementLinkAssets.ts models to use ElementLink instead of TextElementLink for model accuracy

--- angular/assets/elementLinkAssets.ts
+++ angular/assets/elementLinkAssets.ts
@@ -1,9 +1,10 @@
 import {TextElementLink} from "../models/link/TextElementLink";
 import {TargetType} from "../models/html/TargetType";
+import { ElementLink } from "../models/link/ElementLink";
 
-export const navBarHomeElementLink = new TextElementLink('/',
+export const navBarHomeElementLink = new ElementLink('/',
                                                          TargetType.SELF);
-export const navBarSearchElementLink = new TextElementLink('/search',
+export const navBarSearchElementLink = new ElementLink('/search',
                                                            TargetType.SELF);
-export const navBarAccountElementLink = new TextElementLink('/authorize',
+export const navBarAccountElementLink = new ElementLink('/authorize',
                                                             TargetType.SELF);
