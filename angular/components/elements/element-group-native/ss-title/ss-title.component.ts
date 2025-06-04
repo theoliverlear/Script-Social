8b6582eea4b76445e430fe2cc20e52fcd2ffd036
theoliverlear <sigwartholiver2@gmail.com>
2025-06-04 18:22:20 -0500

    Edit ss-title.component.ts to fix import path for `TagType`

--- angular/components/elements/element-group-native/ss-title/ss-title.component.ts
+++ angular/components/elements/element-group-native/ss-title/ss-title.component.ts
@@ -1,6 +1,6 @@
 // ss-title.component.ts 
 import {Component, Input} from "@angular/core";
-import {TagType} from "../../../models/html/TagType";
+import {TagType} from "../../../../models/html/TagType";
 
 @Component({
     selector: 'ss-title',
