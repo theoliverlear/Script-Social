43a0871eeb99821b28da5c01b7b4fa6b5e89d7fc
theoliverlear <sigwartholiver2@gmail.com>
2025-06-04 18:46:52 -0500

    Refactor elements.ts to reformat import statements for better readability and consistency

--- angular/components/elements/elements.ts
+++ angular/components/elements/elements.ts
@@ -1,34 +1,64 @@
-import {AuthBubbleComponent} from "./element-group-auth/auth-bubble/auth-bubble.component";
+import {
+    AuthBubbleComponent
+} from "./element-group-auth/auth-bubble/auth-bubble.component";
 import {
     AuthTypeSelectorComponent
 } from "./element-group-auth/auth-type-selector/auth-type-selector.component";
-import {ConsoleComponent} from "./element-group-console/console/console.component";
-import {ConsoleInputComponent} from "./element-group-console/console-input/console-input.component";
-import {ConsolePopupComponent} from "./element-group-console/console-popup/console-popup.component";
+import {
+    ConsoleComponent
+} from "./element-group-console/console/console.component";
+import {
+    ConsoleInputComponent
+} from "./element-group-console/console-input/console-input.component";
+import {
+    ConsolePopupComponent
+} from "./element-group-console/console-popup/console-popup.component";
 import {
     ConsoleTextboxComponent
 } from "./element-group-console/console-textbox/console-textbox.component";
-import {GeneralPostComponent} from "./element-group-post/element-group-general-post/general-post/general-post.component";
+import {
+    GeneralPostComponent
+} from "./element-group-post/element-group-general-post/general-post/general-post.component";
 import {NavBarComponent} from "./element-group-nav/nav-bar/nav-bar.component";
-import {ProfileBioComponent} from "./element-group-profile/profile-bio/profile-bio.component";
+import {
+    ProfileBioComponent
+} from "./element-group-profile/profile-bio/profile-bio.component";
 import {
     ProfileHeadlineButtonsComponent
 } from "./element-group-profile/profile-headline-buttons/profile-headline-buttons.component";
-import {ProfileNameComponent} from "./element-group-profile/profile-name/profile-name.component";
-import {SideNavBarComponent} from "./element-group-nav/element-group-side-nav/side-nav-bar/side-nav-bar.component";
+import {
+    ProfileNameComponent
+} from "./element-group-profile/profile-name/profile-name.component";
+import {
+    SideNavBarComponent
+} from "./element-group-nav/element-group-side-nav/side-nav-bar/side-nav-bar.component";
 import {
     SideNavBarButtonComponent
 } from "./element-group-nav/element-group-side-nav/side-nav-bar-button/side-nav-bar-button.component";
-import {SsButtonComponent} from "./element-group-native/ss-button/ss-button.component";
-import {SsHeadComponent} from "./element-group-native/ss-head/ss-head.component";
-import {SsImgComponent} from "./element-group-native/ss-img/ss-img.component";
-import {TypingTextComponent} from "./element-group-text/typing-text/typing-text.component";
+import {
+    SsButtonComponent
+} from "./element-group-native/ss-button/ss-button.component";
+import {
+    SsHeadComponent
+} from "./element-group-native/ss-head/ss-head.component";
+import {
+    SsImgComponent
+} from "./element-group-native/ss-img/ss-img.component";
+import {
+    TypingTextComponent
+} from "./element-group-text/typing-text/typing-text.component";
 import {
     ProfileActivityComponent
 } from "./element-group-profile/profile-activity/profile-activity.component";
-import {SsFooterComponent} from "./element-group-native/ss-footer/ss-footer.component";
-import {SsAnchorComponent} from "./element-group-native/ss-anchor/ss-anchor.component";
-import {ContentLinkComponent} from "./element-group-link/content-link/content-link.component";
+import {
+    SsFooterComponent
+} from "./element-group-native/ss-footer/ss-footer.component";
+import {
+    SsAnchorComponent
+} from "./element-group-native/ss-anchor/ss-anchor.component";
+import {
+    ContentLinkComponent
+} from "./element-group-link/content-link/content-link.component";
 import {
     GeneralPostHeaderComponent
 } from "./element-group-post/element-group-general-post/general-post-header/general-post-header.component";
@@ -53,7 +83,9 @@ import {
 import {
     ProfilePictureComponent
 } from "./element-group-profile/profile-picture/profile-picture.component";
-import {SsCodeComponent} from "./element-group-native/ss-code/ss-code.component";
+import {
+    SsCodeComponent
+} from "./element-group-native/ss-code/ss-code.component";
 import {
     MessageBubbleComponent
 } from "./element-group-message/message-bubble/message-bubble.component";
@@ -63,7 +95,9 @@ import {
 import {
     MessagesConnectionsListComponent
 } from "./element-group-message/messages-connections-list/messages-connections-list.component";
-import {MessagesListComponent} from "./element-group-message/messages-list/messages-list.component";
+import {
+    MessagesListComponent
+} from "./element-group-message/messages-list/messages-list.component";
 import {
     MessagesInputComponent
 } from "./element-group-message/messages-input/messages-input.component";
@@ -85,8 +119,12 @@ import {
 import {
     UserChoiceBubbleButtonsComponent
 } from "./element-group-user-choice/user-choice-bubble-buttons/user-choice-bubble-buttons.component";
-import {OptionBubbleComponent} from "./element-group-options/option-bubble/option-bubble.component";
-import {OptionsListComponent} from "./element-group-options/options-list/options-list.component";
+import {
+    OptionBubbleComponent
+} from "./element-group-options/option-bubble/option-bubble.component";
+import {
+    OptionsListComponent
+} from "./element-group-options/options-list/options-list.component";
 import {
     ConnectionsListBubbleComponent
 } from "./element-group-connections/connections-list-bubble/connections-list-bubble.component";
@@ -99,7 +137,12 @@ import {
 import {
     HeadlineButtonComponent
 } from "./element-group-profile/headline-button/headline-button.component";
-import { WelcomeBannerComponent } from "./element-group-welcome/welcome-banner/welcome-banner.component";
+import {
+    WelcomeBannerComponent
+} from "./element-group-welcome/welcome-banner/welcome-banner.component";
+import {
+    SubtitleComponent
+} from "./element-group-text/subtitle/subtitle.component";
 
 export const elements = [
     AuthBubbleComponent,
@@ -146,6 +189,7 @@ export const elements = [
     SsFooterComponent,
     SsHeadComponent,
     SsImgComponent,
+    SubtitleComponent,
     TypingTextComponent,
     UserChoiceBubbleComponent,
     UserChoiceBubbleButtonsComponent,
