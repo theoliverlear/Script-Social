db4b27e3d9a08ac16a81ab856dd71dc565b78671
theoliverlear <sigwartholiver2@gmail.com>
2025-06-04 18:51:41 -0500

    Add blinking-cursor.component.ts to be an Angular component for the blinking-cursor element in the Angular application

---
+++ angular/components/elements/element-group-text/blinking-cursor/blinking-cursor.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'blinking-cursor',
+    templateUrl: './blinking-cursor.component.html',
+    styleUrls: ['./blinking-cursor.component.css']
+})
+export class BlinkingCursorComponent {
+    constructor() {
+
+    }
+}
